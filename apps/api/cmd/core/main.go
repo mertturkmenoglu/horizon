911c1b0052a9cc71775c14694ca2edc13dd097d3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-25 05:28:41 +0300

    use database module

--- apps/api/cmd/core/main.go
+++ apps/api/cmd/core/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"horizon/pkg/db"
 	"horizon/pkg/router"
 	"horizon/pkg/validation"
 	"log"
@@ -28,6 +29,16 @@ func main() {
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORS())
+
+	// Call once to initialize the database connection
+	// Panics if the connection fails
+	db.Init()
+
+	// Call only once to make sure the database schema is up-to-date
+	// All database entities are registered under this function
+	if err := db.AutoMigrate(); err != nil {
+		panic(err.Error())
+	}
 
 	router.Bootstrap(e)
 
