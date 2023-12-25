cc4a9b1d46e103d8380617b14db62a7b21e7f129
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-25 04:55:35 +0300

    add body parser middleware

---
+++ apps/api/pkg/middlewares/parse.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func ParseBody[T any](next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var body T
+
+		if err := c.Bind(&body); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		if err := c.Validate(&body); err != nil {
+			return err
+		}
+
+		c.Set("body", body)
+
+		return next(c)
+	}
+}
