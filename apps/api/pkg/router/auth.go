c7cac3430a1da6167c74e224e8be671054d362e2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-26 09:33:37 +0300

    update auth routes

--- apps/api/pkg/router/auth.go
+++ apps/api/pkg/router/auth.go
@@ -12,28 +12,30 @@ import (
 	"horizon/pkg/jsonwebtoken"
 	"horizon/pkg/password"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
 func Login(c echo.Context) error {
 	body := c.Get("body").(dto.LoginRequest)
 
-	auth, err := query.GetAuthByEmail(body.Email)
+	auth, authErr := query.GetAuthByEmail(body.Email)
 	var hashed = ""
 
 	if auth != nil {
 		hashed = auth.Password
 	}
 
+	user, userErr := query.GetUserByEmail(body.Email)
+
 	// Always calculate the password hash before any early return
 	// to prevent timing based attacks.
 	matched, hashErr := hash.Verify(body.Password, hashed)
 
-	if err != nil || auth == nil {
+	if authErr != nil || userErr != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid email or password")
 	}
 
@@ -41,18 +43,21 @@ func Login(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid email or password")
 	}
 
-	fullName := strings.TrimSpace(fmt.Sprintf("%s %s", auth.FirstName, auth.LastName))
-
 	payload := jsonwebtoken.Payload{
-		AuthId:   auth.Id.String(),
-		UserId:   "",
-		FullName: fullName,
-		Email:    auth.Email,
+		AuthId: auth.Id.String(),
+		UserId: user.Id.String(),
+		Name:   user.Name,
+		Email:  user.Email,
 	}
 
 	expiresAt := time.Now().Add(15 * time.Minute)
 
 	accessToken, err := jsonwebtoken.Encode(payload, expiresAt)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Cannot create token")
+	}
+
 	refreshToken := uuid.New().String()
 	tokenCacheKey := fmt.Sprintf("refreshToken:%s", auth.Id)
 
@@ -91,7 +96,7 @@ func Register(c echo.Context) error {
 	isStrongPassword := password.IsStrong(body.Password)
 
 	// Check if user exists
-	exists, err := query.DoesUserExist(body.Email)
+	exists, err := query.DoesAuthExist(body.Email)
 
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
@@ -101,23 +106,48 @@ func Register(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "Email has already been taken")
 	}
 
+	exists = query.UsernameExists(body.Username)
+
+	if exists {
+		return echo.NewHTTPError(http.StatusBadRequest, "Username has already been taken")
+	}
+
 	if !isStrongPassword {
 		return echo.NewHTTPError(http.StatusBadRequest, "Password is too weak.")
 	}
 
-	auth := models.Auth{
-		FirstName:           "",
-		LastName:            "",
-		Email:               body.Email,
-		OnboardingCompleted: false,
-		EmailVerified:       false,
-		Password:            hashed,
-	}
+	err = db.Client.Transaction(func(tx *gorm.DB) error {
+		auth := models.Auth{
+			EmailVerified: false,
+			Password:      hashed,
+		}
 
-	res := db.Client.Create(&auth)
+		res := tx.Create(&auth)
 
-	if res.Error != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, res.Error.Error())
+		if res.Error != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, res.Error.Error())
+		}
+
+		user := models.User{
+			AuthId:              auth.Id,
+			Name:                body.Name,
+			Email:               body.Email,
+			Username:            body.Username,
+			OnboardingCompleted: false,
+		}
+
+		res = tx.Create(&user)
+
+		if res.Error != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, res.Error.Error())
+		}
+
+		// return nil will commit the whole transaction
+		return nil
+	})
+
+	if err != nil {
+		return err
 	}
 
 	return c.NoContent(http.StatusCreated)
