b62aaa8faabae1607c87c48f6eb173a7faad29ca
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-27 10:01:06 +0300

    add home page

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -6,6 +6,7 @@ const LandingPage = React.lazy(() => import('./pages/LandingPage'));
 const LoginPage = React.lazy(() => import('./pages/LoginPage'));
 const RegisterPage = React.lazy(() => import('./pages/RegisterPage'));
 const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
+const HomePage = React.lazy(() => import('./pages/HomePage'));
 
 export const router = createBrowserRouter([
   {
@@ -20,5 +21,9 @@ export const router = createBrowserRouter([
   {
     path: '/register',
     element: <RegisterPage />,
+  },
+  {
+    path: '/home',
+    element: <HomePage />,
   },
 ]);
