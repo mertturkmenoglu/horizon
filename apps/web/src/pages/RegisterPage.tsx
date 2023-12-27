cb62cb114f753ea3a6381399def84a96658e6af4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-27 13:48:59 +0300

    High in the halls of the kings who are gone

--- apps/web/src/pages/RegisterPage.tsx
+++ apps/web/src/pages/RegisterPage.tsx
@@ -1,26 +1,11 @@
-import FancyGreeting from '@/components/Auth/FancyGreeting';
 import RegisterForm from '@/components/Auth/RegisterForm';
-import Logo from '@/components/Logo';
+import AuthLayout from '@/layouts/AuthLayout';
 
 function RegisterPage(): React.ReactElement {
   return (
-    <div className="w-full h-screen flex">
-      <FancyGreeting className="w-1/2" />
-
-      <div className="w-[1px] bg-gray-500 h-screen" />
-
-      <div className="h-screen w-1/2 flex flex-col justify-center items-start px-64">
-        <div className="min-w-96">
-          <Logo className="w-16 h-16 -ml-4" />
-          <div className="text-2xl font-extrabold mt-16">
-            <div>Hey,</div>
-            <div>Register Now.</div>
-          </div>
-
-          <RegisterForm className="mt-16" />
-        </div>
-      </div>
-    </div>
+    <AuthLayout title="Register Now.">
+      <RegisterForm className="mt-16" />
+    </AuthLayout>
   );
 }
 
