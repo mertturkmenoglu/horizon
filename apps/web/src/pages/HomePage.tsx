38500ea8d67be11b5f24d23888ad988637ae4df9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-27 13:29:53 +0300

    Ne seni görseydim ne de sevseydim

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,4 +1,11 @@
 import Appbar from '@/components/Appbar';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/Carousel';
 import { cn } from '@/lib/cn';
 
 function HomePage(): React.ReactElement {
@@ -30,6 +37,24 @@ function HomePage(): React.ReactElement {
           />
         </div>
       </div>
+
+      <Carousel className="w-full max-w-xs mx-auto">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <div>
+                  <div className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </div>
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
