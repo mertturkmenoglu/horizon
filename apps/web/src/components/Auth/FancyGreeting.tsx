449793d89b74178c9f491c9a429e3351a0f92097
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-27 13:49:10 +0300

    Jenny would dance with her ghosts

--- apps/web/src/components/Auth/FancyGreeting.tsx
+++ apps/web/src/components/Auth/FancyGreeting.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/cn';
+import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 
 interface FancyGreetingProps {
   className?: string;
@@ -12,6 +13,13 @@ function FancyGreeting({ className }: FancyGreetingProps): React.ReactElement {
         className
       )}
     >
+      <a
+        href="/"
+        className="absolute top-8 left-8 flex items-center space-x-4 hover:underline"
+      >
+        <ArrowLeftIcon className="size-6" />
+        <span>Home</span>
+      </a>
       <img
         src="https://images.unsplash.com/photo-1508385082359-f38ae991e8f2?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
