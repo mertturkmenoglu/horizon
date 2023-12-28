ee80ae820293d1217c51e90a89766b9852453233
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-28 16:18:23 +0300

    fix env var prefix bug

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,6 +1,6 @@
 import { ofetch } from 'ofetch';
 
-export const API_URL = import.meta.env.API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = ofetch.create({
   baseURL: API_URL,
