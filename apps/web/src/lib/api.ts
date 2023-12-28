8e44a3d0c419f85f6ade6bca1d4bbdb84a754995
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-28 16:50:05 +0300

    add api error check

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -6,3 +6,17 @@ export const api = ofetch.create({
   baseURL: API_URL,
   credentials: 'include',
 });
+
+export type ApiError = {
+  data: {
+    message: string;
+  };
+  message: string;
+};
+
+export function isApiError(err: unknown): err is ApiError {
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (err as any).data !== undefined
+  );
+}
