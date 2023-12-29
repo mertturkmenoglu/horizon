fc5ff0615850bf2ebea6f6adc75d4d6da031eab3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-29 08:49:08 +0300

    add query client and provider and devtools to root

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,8 +4,11 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { Toaster } from './components/Toaster';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = document.getElementById('root');
+const client = new QueryClient();
 
 if (!root) {
   throw new Error('Root not found');
@@ -13,7 +16,10 @@ if (!root) {
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Toaster />
+    <QueryClientProvider client={client}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+      <Toaster />
+    </QueryClientProvider>
   </React.StrictMode>
 );
