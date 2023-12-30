0e42aa170a90800caae221755440b281aafd349c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-30 08:31:03 +0300

    add email verify banner to main layout

--- apps/web/src/layouts/MainLayout.tsx
+++ apps/web/src/layouts/MainLayout.tsx
@@ -1,5 +1,8 @@
 import Appbar from '@/components/Appbar';
+import Banner from '@/components/Banner';
 import CookieConsent from '@/components/CookieConsent';
+import { useAuth } from '@/hooks/useAuth';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
 
 interface MainLayoutProps {
@@ -7,18 +10,28 @@ interface MainLayoutProps {
 }
 
 function MainLayout({ children }: MainLayoutProps): React.ReactElement {
+  const { isAuthenticated, loading, user } = useAuth();
   const [showCookieConsent, setShowCookieConsent] = useState(() => {
     const lsValue = window.localStorage.getItem('showCookieConsent');
-
-    if (lsValue === null) {
-      return true;
-    }
-
-    return lsValue === 'true';
+    return lsValue === null || lsValue === 'true';
   });
+
+  if (!isAuthenticated || loading || !user) {
+    return <></>;
+  }
+
   return (
     <main>
       <div className="max-w-7xl mx-auto">
+        {!user.emailVerified && (
+          <Banner
+            appearance="warning"
+            className="flex items-center"
+          >
+            <ExclamationTriangleIcon className="size-4" />
+            <div className="ml-2">Please verify your email address</div>
+          </Banner>
+        )}
         <Appbar className="mt-4" />
         <div>{children}</div>
         <CookieConsent
