31174e3620bda98b50452b5cc0c9e2866bd8c457
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-31 12:21:23 +0300

    add dummy new route

--- apps/api/internal/api/v1/router/services.go
+++ apps/api/internal/api/v1/router/services.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"horizon/internal/api/v1/dto"
 	categories "horizon/internal/category"
 	"horizon/internal/h"
 	"net/http"
@@ -9,7 +10,11 @@ import (
 )
 
 func GetServiceCategories(c echo.Context) error {
-	return c.JSON(http.StatusOK, h.Response{
+	return c.JSON(http.StatusOK, h.Response[dto.GetServiceCategoriesRespose]{
 		"data": categories.ServiceCategories,
 	})
+}
+
+func GetServices(c echo.Context) error {
+	return echo.NewHTTPError(http.StatusNotImplemented)
 }
