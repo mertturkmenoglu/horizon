0d9d40e7e032d8d7b955354b4c8e736b72d8860d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-31 08:12:37 +0300

    add config folder

---
+++ apps/api/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+func Bootstrap() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	viper.SetConfigName("dev")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config/")
+	viper.AddConfigPath(".")
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Cannot read configuration file: %s", err.Error()))
+	}
+
+	smtpPassword, ok := os.LookupEnv("SMTP_PASSWORD")
+
+	if !ok {
+		log.Fatal("Set SMTP_PASSWORD variable through environment variables")
+	}
+
+	viper.Set("smtp.password", smtpPassword)
+
+	email, ok := os.LookupEnv("EMAIL")
+
+	if !ok {
+		log.Fatal("Set SMTP_PASSWORD variable through environment variables")
+	}
+
+	viper.Set("email.from", email)
+	viper.Set("smtp.email", email)
+}
