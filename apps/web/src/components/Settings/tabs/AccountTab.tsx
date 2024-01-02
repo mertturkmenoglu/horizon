c3985ad21f1bdb82e4a16648b5807c73890b4657
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-02 17:29:45 +0300

    update settings account tab

--- apps/web/src/components/Settings/tabs/AccountTab.tsx
+++ apps/web/src/components/Settings/tabs/AccountTab.tsx
@@ -1,7 +1,42 @@
+import Input from '@/components/Input';
+import { useAuth } from '@/hooks/useAuth';
+
 function AccountTab(): React.ReactElement {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <></>;
+  }
+
   return (
     <div>
-      <div>Account Tab</div>
+      <h2 className="text-2xl font-semibold">Account Settings</h2>
+      <hr className="w-full h-[2px] bg-black" />
+
+      <div className="max-w-lg mt-4">
+        <Input
+          label="Account ID"
+          value={user.id}
+          hint="This is your unique account ID."
+          disabled
+        />
+
+        <Input
+          label="Email"
+          value={user.email}
+          className="mt-4"
+          hint="You cannot change your email address."
+          disabled
+        />
+
+        <Input
+          label="Username"
+          value={user.username}
+          className="mt-4"
+          hint="You cannot change your username."
+          disabled
+        />
+      </div>
     </div>
   );
 }
