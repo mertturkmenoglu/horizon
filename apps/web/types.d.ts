8b0f6e09a1be4e3e93a0a49b43495e4962b1fb35
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-02 11:26:22 +0300

    add global Tailwind Icon type

---
+++ apps/web/types.d.ts
@@ -0,0 +1,6 @@
+declare type TwIcon = React.ForwardRefExoticComponent<
+  Omit<React.SVGProps<SVGSVGElement>, 'ref'> & {
+    title?: string | undefined;
+    titleId?: string | undefined;
+  } & React.RefAttributes<SVGSVGElement>
+>;
