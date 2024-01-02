f23d8766f5f3f7964f9b4a80037a198eb24b577e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-02 18:34:08 +0300

    prevent home page / landing page flash

--- apps/web/src/pages/LandingPage.tsx
+++ apps/web/src/pages/LandingPage.tsx
@@ -1,7 +1,14 @@
+import { useAuth } from '@/hooks/useAuth';
 import LandingHero from '../components/LandingHero';
 import LandingNavbar from '../components/LandingNavbar';
 
 function LandingPage(): React.ReactElement {
+  const { loading } = useAuth();
+
+  if (loading) {
+    return <></>;
+  }
+
   return (
     <div className="bg-white w-full h-screen flex flex-col items-center">
       <LandingNavbar className="mt-8" />
