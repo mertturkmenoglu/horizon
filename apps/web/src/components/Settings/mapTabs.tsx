9035d2694f7732e5fe0d11fe5a58adb3a18cda8a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-02 17:20:50 +0300

    update settings tabs mapping

--- apps/web/src/components/Settings/mapTabs.tsx
+++ apps/web/src/components/Settings/mapTabs.tsx
@@ -8,7 +8,13 @@ import {
   UserIcon,
 } from '@heroicons/react/24/outline';
 import { TNavItem } from './NavItem';
-import AccountTab from './AccountTab';
+import AccountTab from './tabs/AccountTab';
+import ProfileTab from './tabs/ProfileTab';
+import PreferencesTab from './tabs/PreferencesTab';
+import NotificationsTab from './tabs/NotificationsTab';
+import PrivacyTab from './tabs/PrivacyTab';
+import YourDataTab from './tabs/YourDataTab';
+import HelpTab from './tabs/HelpTab';
 
 export const tabs = [
   {
@@ -20,37 +26,37 @@ export const tabs = [
   {
     id: 'profile',
     name: 'Profile',
-    component: <>Profile</>,
+    component: <ProfileTab />,
     icon: IdentificationIcon,
   },
   {
     id: 'preferences',
     name: 'Preferences',
-    component: <>Preferences</>,
+    component: <PreferencesTab />,
     icon: ComputerDesktopIcon,
   },
   {
     id: 'notifications',
     name: 'Notifications',
-    component: <>Notifications</>,
+    component: <NotificationsTab />,
     icon: BellIcon,
   },
   {
     id: 'privacy',
     name: 'Privacy',
-    component: <>Privacy</>,
+    component: <PrivacyTab />,
     icon: LockClosedIcon,
   },
   {
     id: 'your-data',
     name: 'Your Data',
-    component: <>Your Data</>,
+    component: <YourDataTab />,
     icon: CloudIcon,
   },
   {
     id: 'help',
     name: 'Help',
-    component: <>Help</>,
+    component: <HelpTab />,
     icon: QuestionMarkCircleIcon,
   },
 ] as const satisfies TNavItem[];
