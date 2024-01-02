2196c883cc3194914272b862e79c7ff25f02cd3f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-02 16:50:07 +0300

    add docs to password module

--- apps/api/internal/password/strength.go
+++ apps/api/internal/password/strength.go
@@ -5,11 +5,14 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Returns the zxcvbn entropy score value of the given string s.
 func GetStrength(s string) int {
 	entropyMatch := zxcvbn.PasswordStrength(s, []string{})
 	return entropyMatch.Score
 }
 
+// Checks if the zxcvbn entropy score value of the given string s
+// is greater or equal to minimum security score.
 func IsStrong(s string) bool {
 	strength := GetStrength(s)
 	cmp := viper.GetInt("api.auth.password.min-security")
