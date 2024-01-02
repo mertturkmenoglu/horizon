4180b51b60578265e37554fa141e8fd9c1d79af3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-02 16:49:42 +0300

    add docs to h module

--- apps/api/internal/h/response.go
+++ apps/api/internal/h/response.go
@@ -1,3 +1,4 @@
 package h
 
+// Utility type to wrap arbitrary JSON responses
 type Response[T any] map[string]T
--- apps/api/internal/h/utils.go
+++ apps/api/internal/h/utils.go
@@ -6,6 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// Extracts the cookie with the name cookieName from the Echo context
 func GetCookieFromReq(c echo.Context, cookieName string) *http.Cookie {
 	cookies := c.Request().Cookies()
 
