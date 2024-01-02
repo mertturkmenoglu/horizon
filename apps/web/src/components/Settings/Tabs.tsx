d3802db486cb77ad6271c3341fb076022fe9d8ce
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-02 17:14:31 +0300

    extract settings navigation to a component

---
+++ apps/web/src/components/Settings/Tabs.tsx
@@ -0,0 +1,17 @@
+import { NavItem } from './NavItem';
+import { tabs } from './mapTabs';
+
+export function Tabs(): React.ReactElement {
+  return (
+    <nav className="w-full">
+      <ul className="flex flex-col space-y-1 w-full">
+        {tabs.map((tab) => (
+          <NavItem
+            key={tab.id}
+            {...tab}
+          />
+        ))}
+      </ul>
+    </nav>
+  );
+}
