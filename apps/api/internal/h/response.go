4e7694f98429ce38c5d580537ab5eca90fa86246
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-03 12:53:33 +0300

    add paginated response type

--- apps/api/internal/h/response.go
+++ apps/api/internal/h/response.go
@@ -1,4 +1,11 @@
 package h
 
+import "horizon/internal/pagination"
+
 // Utility type to wrap arbitrary JSON responses
 type Response[T any] map[string]T
+
+type PaginatedResponse[T any] struct {
+	Data       T                     `json:"data"`
+	Pagination pagination.Pagination `json:"pagination"`
+}
