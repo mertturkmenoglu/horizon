7ed30fd65dd49c201231a3ee652dda2fb72e303d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-03 10:53:49 +0300

    update trigger styles

--- apps/web/src/components/NavigationMenu/triggerStyle.tsx
+++ apps/web/src/components/NavigationMenu/triggerStyle.tsx
@@ -1,5 +1,12 @@
+import { cn } from '@/lib/cn';
 import { cva } from 'class-variance-authority';
 
-export const navigationMenuTriggerStyle = cva(
-  'group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50'
+const classes = cn(
+  'group inline-flex h-9 w-max items-center justify-center rounded-md',
+  'bg-white px-4 py-2 text-sm font-medium',
+  'transition-colors hover:bg-neutral-400/10 hover:text-midnight',
+  'focus:ring focus:ring-sky-500 focus:ring-offset-2 focus:outline-none',
+  'disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-neutral-400/20 data-[state=open]:bg-neutral-400/20'
 );
+
+export const navigationMenuTriggerStyle = cva(classes);
