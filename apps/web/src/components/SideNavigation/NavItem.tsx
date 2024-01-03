47f787a5cd799a8611dc38ef2b02c3c1dcd92d6a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-03 16:57:47 +0300

    add nav item component

---
+++ apps/web/src/components/SideNavigation/NavItem.tsx
@@ -0,0 +1,41 @@
+import { cn } from '@/lib/cn';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export type NavItemProps = React.ComponentPropsWithoutRef<'li'> & {
+  href: string;
+  text: string;
+  icon: TwIcon;
+};
+
+const NavItem = React.forwardRef<React.ElementRef<'li'>, NavItemProps>(
+  ({ className, href, text, icon: Icon }, ref) => {
+    return (
+      <li
+        ref={ref}
+        className={cn('list-none', className)}
+      >
+        <NavLink
+          className={({ isActive }) =>
+            cn(
+              'flex items-center space-x-2 py-2 rounded-md px-2',
+              'focus:outline-none focus:ring focus:ring-sky-500',
+              {
+                'bg-sky-100 text-sky-600': isActive,
+                'hover:bg-neutral-400/20': !isActive,
+              }
+            )
+          }
+          to={href}
+        >
+          <Icon className="size-6" />
+          <div>{text}</div>
+        </NavLink>
+      </li>
+    );
+  }
+);
+
+NavItem.displayName = 'NavItem';
+
+export default NavItem;
