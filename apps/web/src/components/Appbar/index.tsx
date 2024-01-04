0fc046f4a2e11a915374fd68c0bc1af9b70b3dc0
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-04 14:17:23 +0300

    update appbar

--- apps/web/src/components/Appbar/index.tsx
+++ apps/web/src/components/Appbar/index.tsx
@@ -1,6 +1,9 @@
-import { cn } from '@/lib/cn';
-import { BellIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
 import Menu from './Menu';
+import Banner from '../Banner';
+import SearchHeader from '../SearchHeader';
+import { SendIcon, BellIcon } from 'lucide-react';
+import CategoryNavigation from '../CategoryNavigation';
+import { cn } from '@/lib/cn';
 
 interface AppbarProps {
   className?: string;
@@ -8,32 +11,37 @@ interface AppbarProps {
 
 function Appbar({ className }: AppbarProps): React.ReactElement {
   return (
-    <div
-      className={cn(
-        ' border-b-2 border-midnight flex justify-between items-center pb-1',
-        className
-      )}
-    >
-      <a
-        href="/"
-        className="text-2xl inline-flex text-midnight"
+    <div className={cn('w-full', className)}>
+      <Banner
+        appearance="announcement"
+        className="flex justify-center items-center py-2"
       >
-        Horizon
-      </a>
+        Welcome to Horizon
+      </Banner>
 
-      <div className="flex items-center space-x-4">
-        <a href="/notifications">
-          <BellIcon className="size-6 text-midnight" />
-          <span className="sr-only">Notifications</span>
-        </a>
+      <div className="flex items-start mt-4 w-full justify-between mx-auto">
+        <div className="flex items-start w-full">
+          <a
+            href="/"
+            className="mt-2 text-midnight text-2xl"
+          >
+            Horizon
+          </a>
+          <SearchHeader className="w-full mx-8" />
+        </div>
 
-        <a href="/messages">
-          <EnvelopeIcon className="size-6 text-midnight" />
-          <span className="sr-only">Messages</span>
-        </a>
+        <div className="flex items-center mt-1.5 space-x-4">
+          <a href="/notifications">
+            <BellIcon className="size-6" />
+          </a>
+          <a href="/messages">
+            <SendIcon className="size-6" />
+          </a>
 
-        <Menu />
+          <Menu />
+        </div>
       </div>
+      <CategoryNavigation className="-ml-2" />
     </div>
   );
 }
