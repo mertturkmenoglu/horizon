c5fb8ec28c48650b741449d925175fe5d17f6bfe
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-04 21:01:01 +0300

    update cta

--- apps/web/src/components/FeaturesCarousel/index.tsx
+++ apps/web/src/components/FeaturesCarousel/index.tsx
@@ -9,7 +9,7 @@ import { cn } from '@/lib/cn';
 
 const items = [
   {
-    text: 'Looking for a yoga instructor? We got you covered.',
+    text: 'Looking for a yoga instructor?',
     image:
       'https://images.unsplash.com/photo-1683130461764-af2bea342b45?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
