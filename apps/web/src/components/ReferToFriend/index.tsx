203ecbdff1b923109d4323642ce794c87ab3d58d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-04 20:35:20 +0300

    update refer to friend styles

--- apps/web/src/components/ReferToFriend/index.tsx
+++ apps/web/src/components/ReferToFriend/index.tsx
@@ -10,18 +10,18 @@ const ReferToFriend = React.forwardRef<El, Props>(
     return (
       <div
         ref={ref}
-        className={cn('flex', className)}
+        className={cn('flex mx-auto max-w-3xl items-center', className)}
         {...props}
       >
-        <Doodle className="size-[500px] text-sky-500" />
+        <Doodle className="size-[256px] text-sky-500" />
         <div className="flex flex-col justify-center ml-32">
-          <div className="text-6xl font-semibold">
+          <div className="text-4xl font-semibold">
             Enjoying <span className="text-sky-500">Horizon</span>?
           </div>
 
           <a
             href="/referral"
-            className="mt-16 text-2xl hover:underline hover:underline-offset-8 hover:decoration-sky-500 hover:decoration-4"
+            className="mt-2 text-lg hover:underline hover:underline-offset-8 hover:decoration-sky-500 hover:decoration-4"
           >
             Refer us to a <span className="text-sky-500">friend</span>.
           </a>
