1e8466998ac858cdbf53fbffec2177be93575d4d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-04 12:58:16 +0300

    update auth hook

--- apps/web/src/hooks/useAuth.tsx
+++ apps/web/src/hooks/useAuth.tsx
@@ -1,25 +1,6 @@
-import { api } from '@/lib/api';
-import { GetMeResponse } from '@/lib/dto';
-import { useEffect, useState } from 'react';
+import { AuthContext } from '@/contexts/AuthContext';
+import { useContext } from 'react';
 
 export function useAuth() {
-  const [checked, setChecked] = useState(false);
-  const [user, setUser] = useState<GetMeResponse | null>(null);
-
-  useEffect(() => {
-    const fetchMe = async () => {
-      const res = await api<{ data: GetMeResponse }>('/users/me');
-      setUser(res.data);
-    };
-
-    fetchMe()
-      .catch(() => setUser(null))
-      .finally(() => setChecked(true));
-  }, [setUser, setChecked]);
-
-  return {
-    isAuthenticated: user !== null,
-    loading: !checked,
-    user,
-  };
+  return useContext(AuthContext);
 }
