61c3180999bfc360c861a0d0bc696648c31d59f5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-04 14:18:10 +0300

    update search component

--- apps/web/src/components/SearchHeader/index.tsx
+++ apps/web/src/components/SearchHeader/index.tsx
@@ -1,15 +1,10 @@
 import { cn } from '@/lib/cn';
-import Logo from '../Logo';
 import ServiceSearch from '../ServiceSearch';
 
 function SearchHeader({ className }: TProps): React.ReactElement {
   return (
-    <div className={cn('pr-4 flex items-start w-full mt-4', className)}>
-      <div className="hidden md:flex items-center space-x-2 mt-2">
-        <Logo className="size-8" />
-        <div>Horizon</div>
-      </div>
-      <ServiceSearch className="ml-4 md:ml-8 w-full" />
+    <div className={cn('flex flex-col items-start w-full', className)}>
+      <ServiceSearch className="mt-0 w-full" />
     </div>
   );
 }
