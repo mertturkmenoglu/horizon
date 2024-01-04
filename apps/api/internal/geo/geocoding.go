a3a458a66c16f6ca5c405c439ebabbba1bfc8854
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-04 18:13:36 +0300

    add geocoding features

---
+++ apps/api/internal/geo/geocoding.go
@@ -0,0 +1,81 @@
+package geo
+
+import (
+	"encoding/json"
+	"os"
+	"slices"
+
+	"github.com/adrg/strutil"
+	"github.com/adrg/strutil/metrics"
+)
+
+type GeoLocation struct {
+	Id      string           `json:"id"`
+	Name    string           `json:"name"`
+	Lat     string           `json:"lat"`
+	Long    string           `json:"long"`
+	Country string           `json:"country"`
+	Admin   GeoLocationAdmin `json:"admin"`
+}
+
+type GeoLocationAdmin struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type SearchResult struct {
+	Similarity float64     `json:"similarity"`
+	Entry      GeoLocation `json:"entry"`
+}
+
+var data []GeoLocation = []GeoLocation{}
+
+const minSimilarity = 0.5
+
+func LoadGeocodingDataFromFile(path string) error {
+	bytes, err := os.ReadFile(path)
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(bytes, &data)
+}
+
+func SearchAll(term string) []SearchResult {
+	similars := make([]SearchResult, 0)
+	m := metrics.NewLevenshtein()
+
+	for _, entry := range data {
+		nameSimScore := strutil.Similarity(term, entry.Name, m)
+		adminSimScore := strutil.Similarity(term, entry.Admin.Name, m)
+		maxSimScore := -1.0
+
+		if adminSimScore > nameSimScore {
+			maxSimScore = adminSimScore
+		} else {
+			maxSimScore = nameSimScore
+		}
+
+		if maxSimScore >= minSimilarity {
+			similars = append(similars, SearchResult{
+				Similarity: maxSimScore,
+				Entry:      entry,
+			})
+		}
+	}
+
+	slices.SortFunc(similars, func(a, b SearchResult) int {
+		if a.Similarity < b.Similarity {
+			return -1
+		}
+
+		if a.Similarity > b.Similarity {
+			return 1
+		}
+
+		return 0
+	})
+
+	return similars[:10]
+}
