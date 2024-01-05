23ae6cf34865d7714b5865cb516b83bb9de8a2f6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-05 15:51:20 +0300

    fix carousel button alignment

--- apps/web/src/components/Carousel/index.tsx
+++ apps/web/src/components/Carousel/index.tsx
@@ -200,7 +200,7 @@ const CarouselPrevious = React.forwardRef<
     <button
       ref={ref}
       className={cn(
-        'absolute  h-8 w-8 rounded-full',
+        'absolute h-8 w-8 rounded-full flex items-center justify-center',
         orientation === 'horizontal'
           ? '-left-12 top-1/2 -translate-y-1/2'
           : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
@@ -227,7 +227,7 @@ const CarouselNext = React.forwardRef<
     <button
       ref={ref}
       className={cn(
-        'absolute h-8 w-8 rounded-full',
+        'absolute h-8 w-8 rounded-full flex items-center justify-center',
         orientation === 'horizontal'
           ? '-right-12 top-1/2 -translate-y-1/2'
           : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
