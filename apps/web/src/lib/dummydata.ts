760e2b28914174c09e3203a401ac6672f97bc09b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-05 11:30:24 +0300

    update dummy data

---
+++ apps/web/src/lib/dummydata.ts
@@ -0,0 +1,174 @@
+import { TRecommendation } from '@/components/RecommendationGrid';
+
+export const newServices: TRecommendation[] = [
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1698434156107-17486c312c50?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Italian teacher at Pavia',
+    username: 'notahu',
+    userImage:
+      'https://fastly.picsum.photos/id/670/2048/1367.jpg?hmac=GT5_zCn_mHuBQWHO0b987AQsuWB_VLmKQJXydYPnSHc',
+    price: '75$',
+    rating: {
+      score: 4.8,
+      reviews: 427,
+    },
+    isNew: false,
+    isPro: true,
+    topRated: true,
+  },
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1594732832278-abd644401426?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Web Design',
+    username: 'janedoe',
+    userImage:
+      'https://fastly.picsum.photos/id/646/2509/1673.jpg?hmac=HXykqhktw0TF08mbS0F3J4bxJJvJRQXG74xA4zPYW7Y',
+    price: '50$',
+    rating: {
+      score: 4.3,
+      reviews: 10,
+    },
+    isNew: true,
+    isPro: false,
+    topRated: false,
+  },
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Biology work group',
+    username: 'metubio',
+    userImage:
+      'https://fastly.picsum.photos/id/447/1280/853.jpg?hmac=4DUUCOsHRIoYbNrPRYEUHOW7wCjM7TROrTrYFivtdPw',
+    price: '5$',
+    rating: {
+      score: 2.9,
+      reviews: 5,
+    },
+    isNew: false,
+    isPro: false,
+    topRated: false,
+  },
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1615906655593-ad0386982a0f?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Car Repair',
+    username: 'tamirciciragi',
+    userImage:
+      'https://fastly.picsum.photos/id/64/4326/2884.jpg?hmac=9_SzX666YRpR_fOyYStXpfSiJ_edO3ghlSRnH2w09Kg',
+    price: '150$',
+    rating: {
+      score: 3.7,
+      reviews: 280,
+    },
+    isNew: false,
+    isPro: true,
+    topRated: false,
+  },
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1616587896595-51352538155b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Wedding Planner',
+    username: 'betul',
+    userImage:
+      'https://fastly.picsum.photos/id/832/1024/1024.jpg?hmac=-RqoKPFzFMlgCiK2vgtkqMp68zsrfbkvzoS2W-gtZjc',
+    price: '120$',
+    rating: {
+      score: 4.9,
+      reviews: 1622,
+    },
+    isNew: false,
+    isPro: false,
+    topRated: true,
+  },
+];
+
+export const populerRightNow: TRecommendation[] = [
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1591448684578-a4cb24ae8861?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Hotwheels Collection Review',
+    username: 'barantheannihilator',
+    userImage: 'https://github.com/barankandil.png',
+    price: '420$',
+    rating: {
+      score: 2.4,
+      reviews: 1,
+    },
+    isNew: false,
+    isPro: false,
+    topRated: false,
+  },
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Mobile App Development',
+    username: 'ogulcan',
+    userImage: 'https://github.com/olcaneristi.png',
+    price: '100$',
+    rating: {
+      score: 4.3,
+      reviews: 936,
+    },
+    isNew: false,
+    isPro: false,
+    topRated: true,
+  },
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1512758017271-d7b84c2113f1?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Scenario Writing Mentoring',
+    username: 'fularlicagdas',
+    userImage:
+      'https://media.licdn.com/dms/image/D4D03AQEImHcpZb3jDw/profile-displayphoto-shrink_800_800/0/1680699396881?e=1709769600&v=beta&t=6CxqdjBufr8CSXgbq4gID4-DyGWnxGhwUzSLVLWHT8U',
+    price: '42$',
+    rating: {
+      score: 5,
+      reviews: 1,
+    },
+    isNew: false,
+    isPro: false,
+    topRated: true,
+  },
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1524178232363-1fb2b075b655?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'German Teacher',
+    username: 'barkaraid',
+    userImage:
+      'https://media.licdn.com/dms/image/D4D03AQG5NjEyF0XEjQ/profile-displayphoto-shrink_800_800/0/1697535877400?e=1709769600&v=beta&t=UqB3UmTr9HwdcApZYB3SFCso5xCjQghTuDR8H4Gfolw',
+    price: '150$',
+    rating: {
+      score: 4.0,
+      reviews: 5,
+    },
+    isNew: true,
+    isPro: true,
+    topRated: false,
+  },
+  {
+    url: '#',
+    image:
+      'https://images.unsplash.com/photo-1533651180995-3b8dcd33e834?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Learn to approach squirrels',
+    username: 'asyncsquirrel',
+    userImage: 'https://github.com/mertturkmenoglu.png',
+    price: '120$',
+    rating: {
+      score: 4.9,
+      reviews: 1622,
+    },
+    isNew: false,
+    isPro: true,
+    topRated: false,
+  },
+];
