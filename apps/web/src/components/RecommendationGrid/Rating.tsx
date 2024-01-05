5e172534eabc4150451a12baef3d1c2c3f2d577c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-05 10:34:06 +0300

    add rating component

---
+++ apps/web/src/components/RecommendationGrid/Rating.tsx
@@ -0,0 +1,19 @@
+import { StarIcon } from '@heroicons/react/24/solid';
+import { TRecommendation } from '.';
+import { cn } from '@/lib/cn';
+
+type RatingProps = TProps & {
+  rating: TRecommendation['rating'];
+};
+
+function Rating({ className, rating }: RatingProps): React.ReactElement {
+  return (
+    <div className={cn('lining-nums flex items-center', className)}>
+      <StarIcon className="size-5 text-sky-500" />
+      <div className="ml-2 font-semibold">{rating.score}</div>
+      <div className="ml-1 text-neutral-500 text-sm">({rating.reviews})</div>
+    </div>
+  );
+}
+
+export default Rating;
