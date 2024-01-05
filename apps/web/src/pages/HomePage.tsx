685ccd91ab061a6ef6c83c2954d4eb6d842d22d5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-05 10:48:50 +0300

    update dummy data

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ const cards: TRecommendation[] = [
     url: '#',
     image:
       'https://images.unsplash.com/photo-1698434156107-17486c312c50?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Italian teacher',
+    title: 'Italian teacher at Pavia',
     username: 'notahu',
     price: '75$',
     rating: {
