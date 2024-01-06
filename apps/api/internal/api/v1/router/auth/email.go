f106b78b1edbbc7e722571b7d2fe0af7254468d3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-06 10:52:51 +0300

    add auth email functions

---
+++ apps/api/internal/api/v1/router/auth/email.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"horizon/internal/geo"
+	"horizon/internal/tasks"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func sendLoginAlertEmail(to string, ip string, useragent string) error {
+	location, err := geo.GetFormattedLocationFromIp(ip)
+
+	t, err := tasks.NewTask[tasks.NewLoginAlertEmailPayload](tasks.TypeNewLoginAlertEmail, tasks.NewLoginAlertEmailPayload{
+		Email:     to,
+		Location:  location,
+		UserAgent: useragent,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	_, err = tasks.Client.Enqueue(t)
+
+	return err
+}
+
+func sendWelcomeEmail(email string, name string) error {
+	t, err := tasks.NewTask[tasks.WelcomeEmailPayload](tasks.TypeWelcomeEmail, tasks.WelcomeEmailPayload{
+		Email: email,
+		Name:  name,
+	})
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	_, err = tasks.Client.Enqueue(t)
+
+	return err
+}
