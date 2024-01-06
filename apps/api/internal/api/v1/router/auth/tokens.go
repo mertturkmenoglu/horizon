d362e90932c68cc7656c43f4fff64e763af4fe52
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-06 10:52:15 +0300

    add auth token related functions

---
+++ apps/api/internal/api/v1/router/auth/tokens.go
@@ -0,0 +1,62 @@
+package auth
+
+import (
+	"fmt"
+	"horizon/internal/api"
+	"horizon/internal/cache"
+	"horizon/internal/db/models"
+	"horizon/internal/jsonwebtoken"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/spf13/viper"
+)
+
+type Tokens struct {
+	AccessToken  string
+	RefreshToken string
+	AccessExp    time.Time
+	RefreshExp   time.Time
+}
+
+func createNewTokens(auth *models.Auth, user *models.User) (*Tokens, error) {
+	access, accExp, accErr := createNewAccessToken(auth, user)
+	refresh, refExp, refErr := createNewRefreshToken(user.Email)
+
+	if accErr != nil || refErr != nil {
+		return nil, api.NewInternalServerError("Cannot create token")
+	}
+
+	return &Tokens{
+		AccessToken:  access,
+		RefreshToken: refresh,
+		AccessExp:    accExp,
+		RefreshExp:   refExp,
+	}, nil
+}
+
+func createNewAccessToken(auth *models.Auth, user *models.User) (string, time.Time, error) {
+	ttl := time.Duration(viper.GetInt64("api.auth.token.access-ttl")) * time.Minute
+	exp := time.Now().Add(ttl)
+	token, err := jsonwebtoken.Encode(jsonwebtoken.Payload{
+		AuthId: auth.Id.String(),
+		UserId: user.Id.String(),
+		Name:   user.Name,
+		Email:  user.Email,
+	}, exp)
+	return token, exp, err
+}
+
+func createNewRefreshToken(email string) (string, time.Time, error) {
+	token := uuid.New().String()
+	key := fmt.Sprintf("refreshToken:%s", token)
+	ttl := time.Minute * time.Duration(viper.GetInt64("api.auth.token.refresh-ttl"))
+	exp := time.Now().Add(ttl)
+	err := cache.Set(key, email, ttl)
+	return token, exp, err
+}
+
+func removeRefreshToken(token string) error {
+	key := fmt.Sprintf("refreshToken:%s", token)
+	return cache.Del(key)
+}
