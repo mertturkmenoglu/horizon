af4631920a5a1e22999497e3d552b7bcc433a23c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-08 01:47:40 +0300

    update features carousel translations

--- apps/web/src/components/FeaturesCarousel/index.tsx
+++ apps/web/src/components/FeaturesCarousel/index.tsx
@@ -6,31 +6,56 @@ import {
   CarouselPrevious,
 } from '@/components/Carousel';
 import { cn } from '@/lib/cn';
+import { useTranslation } from 'react-i18next';
 
-const items = [
+type TItem = {
+  text: Record<string, string>;
+  image: string;
+};
+
+const items: TItem[] = [
   {
-    text: 'Looking for a yoga instructor?',
+    text: {
+      en: 'Looking for a yoga instructor?',
+      tr: 'Yoga eğitmeni mi arıyorsunuz?',
+    },
     image:
       'https://images.unsplash.com/photo-1683130461764-af2bea342b45?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
-    text: 'Need help planning your next trip?',
+    text: {
+      en: 'Need help planning your next trip?',
+      tr: 'Gezinizi planlamak için yardıma mı ihtiyacınız var?',
+    },
     image:
       'https://images.unsplash.com/photo-1529180979161-06b8b6d6f2be?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
-    text: 'Cannot figure out how to publish your website?',
+    text: {
+      en: 'Cannot figure out how to publish your website?',
+      tr: 'Web sitenizi nasıl yayınlayacağınızı bilmiyor musunuz?',
+    },
     image:
       'https://images.unsplash.com/photo-1577909659949-2302826adf26?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
-    text: 'Need help with accounting?',
+    text: {
+      en: 'Need help with accounting?',
+      tr: 'Muhasebe konusunda yardım mı lazım?',
+    },
     image:
       'https://images.unsplash.com/photo-1663524789638-a576264a7f53?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
 ];
 
+const slogan: Record<string, string> = {
+  en: 'We got you covered.',
+  tr: 'Biz yanınızdayız.',
+};
+
 function FeaturesCarousel({ className }: TProps): React.ReactElement {
+  const { i18n } = useTranslation('common');
+
   return (
     <Carousel className={cn('w-full rounded-md', className)}>
       <CarouselContent>
@@ -47,10 +72,10 @@ function FeaturesCarousel({ className }: TProps): React.ReactElement {
                   />
                   <div className="absolute left-2 bottom-2 md:left-16 md:bottom-8 py-4 px-8 rounded-md">
                     <div className="text-white font-bold text-sm md:text-base">
-                      {item.text}
+                      {item.text[i18n.language]}
                     </div>
                     <div className="text-white font-bold text-xl md:text-3xl">
-                      We got you covered.
+                      {slogan[i18n.language]}
                     </div>
                   </div>
                 </div>
