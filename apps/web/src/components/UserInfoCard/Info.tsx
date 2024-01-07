c95d2f9d7be31ef4dea673d0eb75496f4ef9115f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-07 16:19:56 +0300

    update info styles

--- apps/web/src/components/UserInfoCard/Info.tsx
+++ apps/web/src/components/UserInfoCard/Info.tsx
@@ -13,7 +13,12 @@ export function Info(props: InfoProps): React.ReactElement {
   }
 
   return (
-    <div className={cn('w-full flex items-start space-x-2', props.className)}>
+    <div
+      className={cn(
+        'w-full flex items-start space-x-2 lining-nums',
+        props.className
+      )}
+    >
       <props.icon className="size-6 min-w-6 min-h-6 text-sky-600" />
       <div className="text-sm text-wrap wrap break-all">{props.text}</div>
     </div>
