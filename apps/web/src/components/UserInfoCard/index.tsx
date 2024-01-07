95b04f37b39d1e66905d663d80485f4e5cc95fde
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-07 16:20:03 +0300

    update user info card

--- apps/web/src/components/UserInfoCard/index.tsx
+++ apps/web/src/components/UserInfoCard/index.tsx
@@ -3,17 +3,17 @@ import { useUserInfo } from './useUserInfo';
 import { cn } from '@/lib/cn';
 import { Info } from './Info';
 import {
+  ArrowRightIcon,
+  AtSymbolIcon,
   BuildingStorefrontIcon,
   CheckBadgeIcon,
   EnvelopeIcon,
-  HomeIcon,
-  LinkIcon,
   MapPinIcon,
   PencilIcon,
-  PhoneIcon,
   SquaresPlusIcon,
 } from '@heroicons/react/24/outline';
 import Button from '../Button';
+import { VerifiedIcon } from 'lucide-react';
 
 interface UserInfoCardProps {
   user: GetUserByUsernameResponse;
@@ -26,108 +26,113 @@ function UserInfoCard({
 }: UserInfoCardProps): React.ReactElement {
   const { image, description, location, isThisUser } = useUserInfo(user);
   return (
-    <div className={cn('p-4 flex flex-col items-center', className)}>
-      <img
-        src={image}
-        alt="Profile Image"
-        className="size-32 rounded-full ring ring-offset-2 ring-sky-500 mt-8"
-      />
-      <div className="mt-4 text-midnight font-semibold text-xl w-full">
-        {user.name}
-      </div>
-      <div className="text-midnight/70 font-semibold text-sm w-full">
-        @{user.username}
-      </div>
-      <div className="border-y border-y-midnight/50 mt-2 py-2 w-full">
-        <div className="text-sm">{description}</div>
-      </div>
+    <div className={cn('flex flex-col', className)}>
+      <div className="flex justify-between items-start">
+        <div className="flex space-x-8">
+          <img
+            src={image}
+            alt="Profile Image"
+            className="size-36 rounded-full ring ring-offset-2 ring-sky-500"
+          />
+          <div className="">
+            <div className="flex items-center space-x-2">
+              <div className="text-midnight font-semibold text-4xl">
+                {user.name}
+              </div>
 
-      <Info
-        icon={BuildingStorefrontIcon}
-        text="Business Account"
-        className="mt-2"
-        show={user.isBusinessAccount}
-      />
+              {user.isVerifiedAccount && (
+                <VerifiedIcon className="size-6 text-blue-500" />
+              )}
+            </div>
 
-      <Info
-        icon={CheckBadgeIcon}
-        text="Verified Account"
-        className="mt-2"
-        show={user.isVerifiedAccount}
-      />
+            <div className="text-midnight/70 font-semibold text-lg">
+              @{user.username}
+            </div>
 
-      <Info
-        icon={MapPinIcon}
-        text={location}
-        className="mt-2"
-        show={location !== ''}
-      />
+            <div className="flex items-center flex-wrap">
+              <Info
+                icon={SquaresPlusIcon}
+                text="42 services"
+                className="mt-2"
+                show={true}
+              />
 
-      <Info
-        icon={EnvelopeIcon}
-        text={user.contactInformation.email}
-        className="mt-2"
-        show={user.contactInformation.email !== ''}
-      />
+              <Info
+                icon={BuildingStorefrontIcon}
+                text="Business Account"
+                className="mt-2"
+                show={user.isBusinessAccount}
+              />
 
-      <Info
-        icon={PhoneIcon}
-        text={user.contactInformation.phone}
-        className="mt-2"
-        show={user.contactInformation.phone !== ''}
-      />
+              <Info
+                icon={CheckBadgeIcon}
+                text="Verified Account"
+                className="mt-2"
+                show={user.isVerifiedAccount}
+              />
 
-      <Info
-        icon={HomeIcon}
-        text={user.contactInformation.address}
-        className="mt-2"
-        show={user.contactInformation.address !== ''}
-      />
+              <Info
+                icon={MapPinIcon}
+                text={location}
+                className="mt-2"
+                show={location !== ''}
+              />
 
-      <Info
-        icon={LinkIcon}
-        text={user.contactInformation.other}
-        className="mt-2"
-        show={user.contactInformation.other !== ''}
-      />
+              <Info
+                icon={ArrowRightIcon}
+                text={user.gender}
+                className="mt-2"
+                show={user.gender !== ''}
+              />
+            </div>
+          </div>
+        </div>
 
-      <Info
-        icon={SquaresPlusIcon}
-        text="0 services"
-        className="mt-2"
-        show={true}
-      />
-
-      {isThisUser && (
-        <Button
-          appearance="sky"
-          className="mt-4 py-2 flex space-x-2 justify-center items-center w-32"
-          onClick={() => {
-            window.location.href = '/settings?tab=profile';
-          }}
-        >
-          <PencilIcon className="size-5 text-white" />
-          <span>Edit</span>
-        </Button>
-      )}
-
-      {!isThisUser && (
-        <>
+        <div className="flex items-center justify-center flex-col">
           <Button
-            appearance="sky"
-            className="mt-4 py-2 flex space-x-2 justify-center items-center w-32"
+            appearance="midnight"
+            className="py-2 flex space-x-2 justify-center items-center min-w-64"
           >
-            <EnvelopeIcon className="size-5 text-white" />
-            <span>Message</span>
+            <AtSymbolIcon className="size-5 text-white" />
+            <span>View contact info</span>
           </Button>
-          <a
-            href={`/report?u=${user.username}`}
-            className="w-full text-sm mt-2 hover:underline text-midnight/70"
-          >
-            Block or report
-          </a>
-        </>
-      )}
+          {isThisUser && (
+            <Button
+              appearance="midnight"
+              className="mt-2 py-2 flex space-x-2 justify-center items-center min-w-64"
+              onClick={() => {
+                window.location.href = '/settings?tab=profile';
+              }}
+            >
+              <PencilIcon className="size-5 text-white" />
+              <span>Edit</span>
+            </Button>
+          )}
+
+          {!isThisUser && (
+            <>
+              <Button
+                appearance="midnight"
+                className="mt-2 py-2 flex space-x-2 justify-center items-center min-w-64"
+              >
+                <EnvelopeIcon className="size-5 text-white" />
+                <span>Message</span>
+              </Button>
+              <a
+                href={`/report?u=${user.username}`}
+                className="w-full text-sm mt-2 text-center block hover:underline text-midnight/70"
+              >
+                Block or report
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+
+      <div className="mt-8 max-w-2xl">
+        <h2 className="text-xl font-bold">About</h2>
+        <div className="text-lg font-medium mt-1">{description}</div>
+      </div>
     </div>
   );
 }
