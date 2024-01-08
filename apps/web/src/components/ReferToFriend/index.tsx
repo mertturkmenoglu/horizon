c5f3fa19baf40359c83e36ea3338490dc5117128
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-08 03:52:09 +0300

    update refer to friend translations

--- apps/web/src/components/ReferToFriend/index.tsx
+++ apps/web/src/components/ReferToFriend/index.tsx
@@ -1,12 +1,16 @@
 import { cn } from '@/lib/cn';
 import React from 'react';
 import Doodle from './doodle';
+import { Trans, useTranslation } from 'react-i18next';
 
 type El = React.ElementRef<'div'>;
 type Props = React.ComponentPropsWithoutRef<'div'>;
 
 const ReferToFriend = React.forwardRef<El, Props>(
   ({ className, ...props }, ref) => {
+    const { t } = useTranslation('common', {
+      keyPrefix: 'refer-to-friend',
+    });
     return (
       <div
         ref={ref}
@@ -16,14 +20,22 @@ const ReferToFriend = React.forwardRef<El, Props>(
         <Doodle className="size-[256px] text-sky-500" />
         <div className="flex flex-col justify-center ml-32">
           <div className="text-4xl font-semibold">
-            Enjoying <span className="text-sky-500">Horizon</span>?
+            <Trans
+              i18nKey="question"
+              defaults={t('question')}
+              components={{ span: <span className="text-sky-500" /> }}
+            />
           </div>
 
           <a
             href="/referral"
             className="mt-2 text-lg hover:underline hover:underline-offset-8 hover:decoration-sky-500 hover:decoration-4 rounded-md"
           >
-            Refer us to a <span className="text-sky-500">friend</span>.
+            <Trans
+              i18nKey="cta"
+              defaults={t('cta')}
+              components={{ span: <span className="text-sky-500" /> }}
+            />
           </a>
         </div>
       </div>
