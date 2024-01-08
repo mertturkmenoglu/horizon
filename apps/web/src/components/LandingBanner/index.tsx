7946f82ad3e274278d9010c3024dec36e87d751b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-08 03:12:12 +0300

    update landing banner translations

--- apps/web/src/components/LandingBanner/index.tsx
+++ apps/web/src/components/LandingBanner/index.tsx
@@ -1,24 +1,26 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Banner from '../Banner';
+import { useTranslation } from 'react-i18next';
 
 function LandingBanner(): React.ReactElement {
+  const { t } = useTranslation('landing', { keyPrefix: 'banner ' });
   return (
     <Banner
       appearance="announcement"
       className="flex justify-center items-baseline"
     >
       <div className="bg-sky-600 rounded-full text-white text-sm font-bold px-3 py-0.5">
-        New
+        {t('new')}
       </div>
       <div className="py-1 text-base font-medium text-midnight ml-4">
-        Introducing Horizon - your new address to find services.
+        {t('text')}
       </div>
       <div className="ml-4">
         <a
           href="/register"
           className="font-bold text-midnight flex items-center text-base"
         >
-          Sign up now
+          {t('cta')}
           <ArrowRightIcon className="size-4 ml-1" />
         </a>
       </div>
