f443f716fba433e5d4cfda03b2cafa432aedcd82
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-08 14:58:42 +0300

    configure vitest

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,18 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
   },
 });
