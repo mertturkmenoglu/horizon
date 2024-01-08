87fefb5f5db6fed552d13703c340d62a8a44b97c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-08 18:49:20 +0300

    setup body dump and jaegertracing middlewares

--- apps/api/cmd/core/main.go
+++ apps/api/cmd/core/main.go
@@ -11,6 +11,7 @@ import (
 	"horizon/internal/validation"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo-contrib/jaegertracing"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
@@ -28,6 +29,10 @@ func main() {
 
 	e.Use(middleware.Recover())
 
+	// Enable tracing middleware
+	c := jaegertracing.New(e, nil)
+	defer c.Close()
+
 	if viper.GetString("env") == "dev" {
 		// Extracts the IP address directly from request.
 		// If the application doesn't face the web directly
@@ -39,12 +44,21 @@ func main() {
 			Format: viper.GetString("api.logger.format"),
 		}))
 
+		e.Use(middleware.RequestID())
+
 		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 			AllowOrigins:                             middleware.DefaultCORSConfig.AllowOrigins,
 			AllowMethods:                             middleware.DefaultCORSConfig.AllowMethods,
 			AllowHeaders:                             middleware.DefaultCORSConfig.AllowHeaders,
 			AllowCredentials:                         true,
 			UnsafeWildcardOriginWithAllowCredentials: true,
+		}))
+
+		e.Use(middleware.BodyDump(func(ctx echo.Context, b1, b2 []byte) {
+			sp := jaegertracing.CreateChildSpan(ctx, ctx.Response().Header().Get(echo.HeaderXRequestID))
+			defer sp.Finish()
+			sp.SetBaggageItem("Request", string(b1))
+			sp.SetBaggageItem("Response", string(b2))
 		}))
 	}
 
