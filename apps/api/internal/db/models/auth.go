8258ac8f4d08b59cb8c58cfb3920643b13a42041
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-09 01:56:57 +0300

    add activity type to auth activity model

--- apps/api/internal/db/models/auth.go
+++ apps/api/internal/db/models/auth.go
@@ -11,13 +11,21 @@ type Auth struct {
 	Password string `json:"-"`
 }
 
+// Activity type values are
+//
+// 0: Login Attempt
+// 1: Logout
+// 2: Password change
+// 3: Password reset
+// 4: Email verification
 type AuthActivity struct {
-	Id        uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
-	AuthId    uuid.UUID `json:"-" gorm:"index;not null"`
-	Auth      Auth      `json:"-"`
-	CreatedAt time.Time `json:"createdAt"`
-	IpAddress string    `json:"ipAddress"`
-	UserAgent string    `json:"userAgent"`
-	Location  string    `json:"location"`
-	Success   bool      `json:"success"`
+	Id           uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
+	AuthId       uuid.UUID `json:"-" gorm:"index;not null"`
+	Auth         Auth      `json:"-"`
+	ActivityType int       `json:"activityType" gorm:"not null;size:8;default:0"`
+	CreatedAt    time.Time `json:"createdAt"`
+	IpAddress    string    `json:"ipAddress"`
+	UserAgent    string    `json:"userAgent"`
+	Location     string    `json:"location"`
+	Success      bool      `json:"success"`
 }
