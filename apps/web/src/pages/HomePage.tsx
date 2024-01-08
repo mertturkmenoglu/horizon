ec1c64fadf4a3aebc4bd395a7558938ab5385e6d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-08 05:16:03 +0300

    update home page translations

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -5,14 +5,19 @@ import ReferToFriend from '@/components/ReferToFriend';
 import { useAuth } from '@/hooks/useAuth';
 import MainLayout from '@/layouts/MainLayout';
 import { newServices, populerRightNow } from '@/lib/dummydata';
+import { useTranslation } from 'react-i18next';
 
 function HomePage(): React.ReactElement {
+  const { t } = useTranslation('home');
   const { user } = useAuth();
+
   return (
     <MainLayout>
       <ReferToFriend className="mt-0" />
-      <h2 className="mt-8 text-4xl font-bold">Hey {user?.name},</h2>
-      <div className="mt-2 text-lg font-bold">How can we help you?</div>
+      <h2 className="mt-8 text-4xl font-bold">
+        {t('title', { name: user?.name ?? '' })}
+      </h2>
+      <div className="mt-2 text-lg font-bold">{t('subtitle')}</div>
 
       <QuickActions className="mt-8" />
 
