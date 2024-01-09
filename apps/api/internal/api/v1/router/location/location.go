b8a58eaeb938a95feb5725f2c90de0d5474cb831
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-09 15:13:27 +0300

    add location caching

--- apps/api/internal/api/v1/router/location/location.go
+++ apps/api/internal/api/v1/router/location/location.go
@@ -2,17 +2,27 @@ package location
 
 import (
 	"horizon/internal/api/v1/dto"
+	"horizon/internal/cache"
 	"horizon/internal/geo"
 	"horizon/internal/h"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 func SearchLocation(c echo.Context) error {
 	term := c.QueryParam("term")
+	res, err := cache.GetObj[[]geo.SearchResult]("location-search:" + term)
+
+	if err == nil {
+		return c.JSON(http.StatusOK, h.Response[dto.SearchLocationResponse]{
+			"data": *res,
+		})
+	}
 
 	results := geo.SearchAll(term)
+	cache.SetObj[[]geo.SearchResult]("location-search:"+term, results, time.Minute*10)
 
 	return c.JSON(http.StatusOK, h.Response[dto.SearchLocationResponse]{
 		"data": results,
