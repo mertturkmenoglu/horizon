f5388909bc0c2908b075125ef24c18a52c0cc67b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-10 09:14:42 +0300

    add million.js to project

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
+import million from 'million/compiler';
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
