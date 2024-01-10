137e5f47d298ea1522a8028c6846f7f7ee57ac38
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-10 10:19:02 +0300

    refactor locale module

--- apps/api/internal/locale/locale.go
+++ apps/api/internal/locale/locale.go
@@ -9,24 +9,31 @@ import (
 	"golang.org/x/text/language"
 )
 
-var bundle *i18n.Bundle
+type Locale struct {
+	Bundle *i18n.Bundle
+}
 
-func New() {
-	bundle = i18n.NewBundle(language.English)
-	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+func New() *Locale {
+	l := Locale{}
+	l.Bundle = i18n.NewBundle(language.English)
+	l.Bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
 	pwd, err := os.Getwd()
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	currDir := path.Join(pwd, "internal", "locale")
-	bundle.MustLoadMessageFile(path.Join(currDir, "active.en.toml"))
-	bundle.MustLoadMessageFile(path.Join(currDir, "active.tr.toml"))
+	l.Bundle.MustLoadMessageFile(path.Join(currDir, "active.en.toml"))
+	l.Bundle.MustLoadMessageFile(path.Join(currDir, "active.tr.toml"))
+
+	return &l
 }
 
-func Localizer(langs ...string) *i18n.Localizer {
-	if bundle == nil {
+func (l *Locale) Localizer(langs ...string) *i18n.Localizer {
+	if l.Bundle == nil {
 		panic("i18n bundle not initialized")
 	}
 
-	return i18n.NewLocalizer(bundle, langs...)
+	return i18n.NewLocalizer(l.Bundle, langs...)
 }
