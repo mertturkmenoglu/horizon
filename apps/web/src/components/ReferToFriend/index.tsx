12bcd5dcf7baba4d542c73d97e5268ffb8994d88
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-10 11:47:16 +0300

    update colors for a11y color contrast ratio requirements

--- apps/web/src/components/ReferToFriend/index.tsx
+++ apps/web/src/components/ReferToFriend/index.tsx
@@ -17,24 +17,24 @@ const ReferToFriend = React.forwardRef<El, Props>(
         className={cn('flex max-w-3xl items-center', className)}
         {...props}
       >
-        <Doodle className="size-[256px] text-sky-500" />
+        <Doodle className="size-[256px] text-sky-600" />
         <div className="ml-32 flex flex-col justify-center">
           <div className="text-4xl font-semibold">
             <Trans
               i18nKey="question"
               defaults={t('question')}
-              components={{ span: <span className="text-sky-500" /> }}
+              components={{ span: <span className="text-sky-600" /> }}
             />
           </div>
 
           <a
             href="/referral"
-            className="mt-2 rounded-md text-lg hover:underline hover:decoration-sky-500 hover:decoration-4 hover:underline-offset-8"
+            className="mt-2 rounded-md text-lg font-medium hover:underline hover:decoration-sky-700 hover:decoration-4 hover:underline-offset-8"
           >
             <Trans
               i18nKey="cta"
               defaults={t('cta')}
-              components={{ span: <span className="text-sky-500" /> }}
+              components={{ span: <span className="text-sky-700" /> }}
             />
           </a>
         </div>
