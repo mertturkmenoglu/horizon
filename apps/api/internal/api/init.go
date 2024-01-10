cad3212367601327fcc98eab8f30f3565baf7d1c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-11 00:01:28 +0300

    initalize sonyflake in app module

--- apps/api/internal/api/init.go
+++ apps/api/internal/api/init.go
@@ -5,6 +5,7 @@ import (
 	"horizon/internal/geo"
 	"horizon/internal/locale"
 
+	"github.com/sony/sonyflake"
 	"github.com/spf13/viper"
 )
 
@@ -12,6 +13,7 @@ type AppModule struct {
 	Geo    *geo.GeoCoding
 	Locale *locale.Locale
 	Cache  *cache.Cache
+	Flake  *sonyflake.Sonyflake
 }
 
 var App *AppModule
@@ -21,6 +23,7 @@ func Init() {
 		Geo:    &geo.GeoCoding{},
 		Locale: locale.New(),
 		Cache:  cache.New(),
+		Flake:  nil,
 	}
 
 	// Init ip2location
@@ -28,4 +31,13 @@ func Init() {
 
 	// Read geocoding data
 	App.Geo.LoadGeocodingDataFromFile(viper.GetString("api.geo.geocode"))
+
+	// Initialize Sonyflake
+	flake, err := sonyflake.New(sonyflake.Settings{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	App.Flake = flake
 }
