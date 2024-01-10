ac3956fb06db340482b14472f0f4d8b3a2cb6782
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-11 01:13:32 +0300

    update service category component

--- apps/web/src/components/ServiceCategory/index.tsx
+++ apps/web/src/components/ServiceCategory/index.tsx
@@ -1,34 +1,10 @@
 import { cn } from '@/lib/cn';
 import { ServiceCategory as ServiceCategoryDto } from '@/lib/dto';
+import CategoryCard from '../CategoryCard';
 
 interface ServiceCategoryProps {
   cat: ServiceCategoryDto;
   className?: string;
-}
-
-function CategoryCard({
-  id,
-  title,
-  className,
-}: {
-  id: number;
-  title: string;
-  className?: string;
-}): React.ReactElement {
-  return (
-    <a
-      href={`/services?category=${id}`}
-      className={cn(
-        'border border-midnight p-4',
-        'flex size-48 items-center justify-start rounded-md',
-        'focus:border-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2',
-        'group',
-        className
-      )}
-    >
-      <div className="text-midnight">{title}</div>
-    </a>
-  );
 }
 
 function ServiceCategory({
@@ -38,12 +14,11 @@ function ServiceCategory({
   return (
     <div className={cn('', className)}>
       <h2 className="text-2xl font-bold">{cat.category}</h2>
-      <div className="mt-4 flex space-x-4">
+      <div className="mt-4 grid grid-cols-4 gap-4">
         {cat.subcategories.map((c) => (
           <CategoryCard
             key={c.id}
-            id={c.id}
-            title={c.title}
+            category={c.title}
           />
         ))}
       </div>
