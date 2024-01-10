bf41a94d6ffc3edc773c79521d42352b54e779c4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-11 00:01:05 +0300

    add service models

---
+++ apps/api/internal/db/models/service.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Service struct {
+	Id               uint64    `gorm:"not null;uniqueIndex"`
+	CreatedAt        time.Time ``
+	UpdatedAt        time.Time `gorm:"default:null"`
+	DeletedAt        time.Time `gorm:"default:null"`
+	User             User      ``
+	UserId           uuid.UUID `gorm:"type:uuid;not null;index"`
+	Title            string    `gorm:"not null;size:64;default:'';index"`
+	Slug             string    `gorm:"not null;size:64;default:'';index"`
+	Description      string    `gorm:"not null;size:4096;default:''"`
+	Category         int       `gorm:"not null"`
+	Price            string    `gorm:"not null;size:10;default:''"`
+	PriceUnit        string    `gorm:"not null;size:5;default:''"`
+	PriceTimespan    int       `gorm:"not null;default:0"`
+	IsOnline         bool      `gorm:"not null;default:false"`
+	Location         string    `gorm:"not null;size:128;default:''"`
+	DeliveryTime     int       `gorm:"not null;default:1"`
+	DeliveryTimespan int       `gorm:"not null;default:0"`
+	Photos           []ServicePhoto
+	Videos           []ServiceVideo
+}
+
+type ServicePhoto struct {
+	BaseModel
+	ServiceId uuid.UUID `gorm:"type:uuid;not null;index"`
+	Url       string    `gorm:"not null"`
+	Alt       string    `gorm:"not null;size:128"`
+}
+
+type ServiceVideo struct {
+	BaseModel
+	ServiceId uuid.UUID `gorm:"type:uuid;not null;index"`
+	Url       string    `gorm:"not null"`
+	Alt       string    `gorm:"not null;size:128"`
+}
