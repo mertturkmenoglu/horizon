ffbb54e58cefa65b54e3ba5fc18d5ce9e67b4d9d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-11 03:39:09 +0300

    add helmet provider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -8,6 +8,7 @@ import { Toaster } from './components/Toaster';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AuthContextProvider } from './contexts/AuthContextProvider';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = document.getElementById('root');
 const client = new QueryClient();
@@ -19,11 +20,13 @@ if (!root) {
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <AuthContextProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <RouterProvider router={router} />
-        <Toaster />
-      </AuthContextProvider>
+      <HelmetProvider>
+        <AuthContextProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <RouterProvider router={router} />
+          <Toaster />
+        </AuthContextProvider>
+      </HelmetProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
