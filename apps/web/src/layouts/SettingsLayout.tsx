49acd17b01b92b07a99c8c62a7483bb7ab327a49
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-13 17:11:27 +0300

    remove settings layout divider

--- apps/web/src/layouts/SettingsLayout.tsx
+++ apps/web/src/layouts/SettingsLayout.tsx
@@ -10,7 +10,7 @@ function SettingsLayout({
   return (
     <div className="mt-8 grid grid-cols-12">
       <div className="col-span-12 lg:col-span-2">{tabs}</div>
-      <div className="col-span-12 mt-8 px-4 lg:col-span-10 lg:mt-0 lg:border-l lg:border-l-midnight">
+      <div className="col-span-12 mt-8 px-4 lg:col-span-10 lg:mt-0">
         {children}
       </div>
     </div>
