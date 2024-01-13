3ff4d65b5a6f8db4c63177d6ba27c96baacf3f48
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-14 01:21:14 +0300

    update category card

--- apps/web/src/components/CategoryCard/index.tsx
+++ apps/web/src/components/CategoryCard/index.tsx
@@ -4,23 +4,26 @@ import { Link } from 'react-router-dom';
 
 type Props = React.ComponentPropsWithoutRef<'a'> & {
   category: string;
+  img: string;
 };
 
-function CategoryCard({ className, category, ...props }: Props) {
+function CategoryCard({ className, category, img, ...props }: Props) {
   const { t } = useTranslation('common', { keyPrefix: 'browse-categories' });
   const href = `/categories/${encodeURIComponent(category)}`;
 
   return (
     <Link
       to={href}
-      className={cn(
-        'rounded-md bg-gradient-to-br from-yellow-300 to-yellow-400 p-4 hover:bg-gradient-to-r',
-        className
-      )}
+      className={cn('rounded-md', className)}
       {...props}
     >
-      <div className="text-xl font-bold text-midnight">{category}</div>
-      <div className="mb-4 mt-4 flex items-baseline space-x-2 md:mb-8">
+      <img
+        src={img}
+        alt=""
+        className="aspect-[2] rounded-t-md object-cover"
+      />
+      <div className="mt-2 text-xl font-bold">{category}</div>
+      <div className="mb-4 mt-1 flex items-baseline space-x-2 md:mb-8">
         <div className="text-sm">{t('num-services', { count: 0 })}</div>
       </div>
     </Link>
