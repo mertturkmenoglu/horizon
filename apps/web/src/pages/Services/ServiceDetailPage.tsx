e30a475cccb30562185c3b8d7d7bb0b81db319ab
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-14 17:41:11 +0300

    update service detail page

--- apps/web/src/pages/Services/ServiceDetailPage.tsx
+++ apps/web/src/pages/Services/ServiceDetailPage.tsx
@@ -1,16 +1,23 @@
+import Breadcrumb, { TBreadcrumbItem } from '@/components/Breadcrumb';
 import Spinner from '@/components/Spinner';
+import { useCategoryData } from '@/hooks/useCategoryData';
 import MainLayout from '@/layouts/MainLayout';
 import { api } from '@/lib/api';
+import { getCurrencySymbolOrDefault } from '@/lib/currency';
 import { GetServiceByIdResponse } from '@/lib/dto/service';
+import { getUserImage } from '@/lib/img';
 import { useQuery } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
-function ServiceDetailPage(): React.ReactElement {
+function Container(): React.ReactElement {
   const { id } = useParams();
   const query = useQuery({
     queryKey: ['service', id],
     queryFn: async () => {
-      return await api<{ data: GetServiceByIdResponse }>('/services/' + id);
+      const res = await api<{ data: GetServiceByIdResponse }>(
+        '/services/' + id
+      );
+      return res.data;
     },
     enabled: !!id,
     refetchOnWindowFocus: false,
@@ -35,13 +42,84 @@ function ServiceDetailPage(): React.ReactElement {
     );
   }
 
+  if (!query.data) {
+    return <></>;
+  }
+
+  return <ServiceDetailPage service={query.data} />;
+}
+
+type Props = {
+  service: GetServiceByIdResponse;
+};
+
+const timespanToText: Record<number, string> = {
+  0: 'Hour',
+  1: 'Day',
+  2: 'Week',
+  3: 'Month',
+};
+
+function ServiceDetailPage({ service }: Props): React.ReactElement {
+  const categoryData = useCategoryData();
+  const flattenCategories = categoryData.data
+    .map((c) => [c, c.subcategories].flat())
+    .flat();
+
+  const category =
+    flattenCategories.find((c) => c.id === service.category) ?? null;
+
+  const breadcrumbItems: TBreadcrumbItem[] = [
+    {
+      href: `/categories/${encodeURIComponent(
+        category?.title ?? ''
+      )}?id=${category?.id}`,
+      text: category?.title ?? '',
+      capitalize: true,
+    },
+    {
+      href: '#',
+      text: service.title.toLocaleLowerCase() ?? '',
+      capitalize: true,
+    },
+  ];
+
   return (
     <MainLayout>
-      <div>Service Detail Page</div>
-      <div>id: {id}</div>
-      {query.data && <pre>{JSON.stringify(query.data.data, null, 2)}</pre>}
+      <Breadcrumb
+        items={breadcrumbItems}
+        className="mt-8"
+      />
+
+      <div className="mt-8 flex justify-between">
+        <div>
+          <h2 className="text-3xl font-bold">{service.title}</h2>
+          <pre className="mt-8 max-w-4xl text-wrap break-normal font-sans text-lg">
+            {service.description.replace(/\t/g, '\n')}
+          </pre>
+        </div>
+
+        <Link
+          to={`/user/${service.user.username}`}
+          className="flex flex-col items-end"
+        >
+          <img
+            src={getUserImage(service.user.profileImage)}
+            alt=""
+            className="size-24 rounded-lg"
+          />
+          <div className="mt-4 text-2xl font-medium">{service.user.name}</div>
+          <div className="text-midnight/70">@{service.user.username}</div>
+          <div className="mt-4 text-lg font-bold">
+            {service.price} {getCurrencySymbolOrDefault(service.priceUnit)} /{' '}
+            {timespanToText[service.priceTimespan]}
+          </div>
+        </Link>
+      </div>
+
+      <pre className="mt-32 text-wrap">{JSON.stringify(service, null, 2)}</pre>
     </MainLayout>
   );
 }
 
-export default ServiceDetailPage;
+export default Container;
