cf74dc7451684749e6a76e8898cc74d3d7a0da71
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-14 14:14:25 +0300

    add dummy help page

--- apps/web/src/pages/HelpPage.tsx
+++ apps/web/src/pages/HelpPage.tsx
@@ -1,13 +1,48 @@
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/Accordion';
+import Breadcrumb from '@/components/Breadcrumb';
+import MainLayout from '@/layouts/MainLayout';
+import { useTranslation } from 'react-i18next';
 
 function HelpPage(): React.ReactElement {
+  const { t } = useTranslation('help');
+
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
-      <ExclamationTriangleIcon className="size-24 text-red-500" />
-      <div className="text-2xl">
-        We are working on this page. Come back later.
+    <MainLayout>
+      <Breadcrumb
+        items={[{ href: '/help', text: t('breadcrumb.title') }]}
+        className="mt-8"
+      />
+
+      <h2 className="mt-16 text-4xl font-bold text-midnight">{t('title')}</h2>
+
+      <h3
+        id="faq"
+        className="mt-16 text-xl font-bold"
+      >
+        {t('faq.title')}
+      </h3>
+
+      <div className="max-w-2xl">
+        <Accordion
+          type="multiple"
+          className="mt-4"
+        >
+          {new Array(10).fill(0).map((_, i) => (
+            <AccordionItem value={`item-${i + 1}`}>
+              <AccordionTrigger>{t(`faq.items.${i}.title`)}</AccordionTrigger>
+              <AccordionContent>{t(`faq.items.${i}.content`)}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
-    </div>
+
+      <div className="grid grid-cols-3"></div>
+    </MainLayout>
   );
 }
 
