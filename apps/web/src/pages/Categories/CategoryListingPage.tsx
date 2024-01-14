851af4202913410f8750680a7b9a3fdfb8189157
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-14 13:44:31 +0300

    add dummy category listing page

---
+++ apps/web/src/pages/Categories/CategoryListingPage.tsx
@@ -0,0 +1,13 @@
+import MainLayout from '@/layouts/MainLayout';
+
+function CategoryListingPage(): React.ReactElement {
+  return (
+    <MainLayout>
+      <div>
+        <div>Category listing page</div>
+      </div>
+    </MainLayout>
+  );
+}
+
+export default CategoryListingPage;
