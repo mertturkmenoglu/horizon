46dda255c1b4819482abdedcb6911b32e6e29e55
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-15 11:21:54 +0300

    add service count prop to category card component

--- apps/web/src/components/CategoryCard/index.tsx
+++ apps/web/src/components/CategoryCard/index.tsx
@@ -6,9 +6,17 @@ type Props = Omit<React.ComponentPropsWithoutRef<'a'>, 'id'> & {
   category: string;
   img: string;
   id: number;
+  count: number;
 };
 
-function CategoryCard({ className, category, img, id, ...props }: Props) {
+function CategoryCard({
+  className,
+  category,
+  img,
+  id,
+  count,
+  ...props
+}: Props) {
   const { t } = useTranslation('common', { keyPrefix: 'browse-categories' });
   const href = `/categories/${encodeURIComponent(category)}?id=${id}`;
 
@@ -25,7 +33,7 @@ function CategoryCard({ className, category, img, id, ...props }: Props) {
       />
       <div className="mt-2 text-xl font-bold">{category}</div>
       <div className="mb-4 mt-1 flex items-baseline space-x-2 md:mb-8">
-        <div className="text-sm">{t('num-services', { count: 0 })}</div>
+        <div className="text-sm">{t('num-services', { count })}</div>
       </div>
     </Link>
   );
