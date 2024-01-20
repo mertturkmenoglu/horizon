d9355b1e52a3afad54c88e1c28ea2154d094d3cd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-20 21:04:18 +0300

    update favorites indices

--- apps/api/internal/db/models/favorites.go
+++ apps/api/internal/db/models/favorites.go
@@ -2,6 +2,7 @@ package models
 
 type Favorite struct {
 	BaseModel
-	ServiceId string `gorm:"not null;uniqueIndex:fav_idx"`
-	UserId    string `gorm:"not null;uniqueIndex:fav_idx"`
+	ServiceId string  `gorm:"not null;uniqueIndex:fav_idx"`
+	Service   Service ``
+	UserId    string  `gorm:"not null;uniqueIndex:fav_idx;index:fav_user_idx"`
 }
