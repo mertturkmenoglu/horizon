3a294def8b939b6ff4c117dd9e9e0d713d4993f0
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-20 17:16:57 +0300

    add help option to fake data generator

--- apps/api/cmd/fake/main.go
+++ apps/api/cmd/fake/main.go
@@ -14,11 +14,21 @@ func main() {
 		log.Fatalln("Give a flag")
 	}
 
+	if *f == "help" || *f == "h" {
+		flag.Usage()
+		return
+	}
+
 	if *f == "users" {
 		gen_users(*n, "tmp/fake_data.json")
+		return
 	}
 
 	if *f == "services" {
 		gen_services(*n, "tmp/fake_services_data.json")
+		return
 	}
+
+	flag.Usage()
+	log.Fatalln("Give a flag")
 }
