34968f8254b5a58a976fa2f49ebc7e681be77141
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-01-24 10:20:43 +0300

    update auth layout styles

--- apps/web/src/layouts/AuthLayout.tsx
+++ apps/web/src/layouts/AuthLayout.tsx
@@ -1,4 +1,3 @@
-import FancyGreeting from '@/components/Auth/FancyGreeting';
 import Logo from '@/components/Logo';
 
 interface AuthLayoutProps {
@@ -8,15 +7,11 @@ interface AuthLayoutProps {
 
 function AuthLayout({ children, title }: AuthLayoutProps): React.ReactElement {
   return (
-    <main className="flex h-screen w-full flex-col lg:flex-row">
-      <FancyGreeting className=" lg:w-1/2" />
-
-      <div className="hidden h-screen w-[1px] bg-gray-500 lg:flex" />
-
-      <div className="mx-auto mt-16 flex flex-col items-start justify-center lg:mt-0 lg:h-screen lg:w-1/2 lg:px-32 xl:px-64">
+    <main className="mt-16 flex w-full flex-col lg:flex-row">
+      <div className="mx-auto flex flex-col items-start justify-center">
         <div className="min-w-96">
           <Logo className="-ml-4 h-16 w-16" />
-          <div className="mt-16 text-2xl font-extrabold">
+          <div className="mt-4 text-2xl font-extrabold">
             <div>Hey,</div>
             <h1>{title}</h1>
           </div>
