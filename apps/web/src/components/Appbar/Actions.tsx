f8eeb569a2ea6ecc0c13a3db347fa8a534cf0032
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-02-08 15:57:02 +0300

    refactor appbar actions

--- apps/web/src/components/Appbar/Actions.tsx
+++ apps/web/src/components/Appbar/Actions.tsx
@@ -6,37 +6,40 @@ import { Link } from 'react-router-dom';
 function Actions(): React.ReactElement {
   const { t } = useTranslation('appbar');
 
+  const actions = [
+    {
+      icon: PlusIcon,
+      label: t('create-service'),
+      to: '/services/new',
+    },
+    {
+      icon: BellIcon,
+      label: t('notifications'),
+      to: '/notifications',
+    },
+    {
+      icon: EnvelopeIcon,
+      label: t('messages'),
+      to: '/messages',
+    },
+  ];
+
   return (
     <>
-      <Tooltip content={t('create-service')}>
-        <Link
-          to="/services/new"
-          className="rounded-full hover:bg-neutral-400/20"
+      {actions.map((action) => (
+        <Tooltip
+          key={action.to}
+          content={action.label}
         >
-          <PlusIcon className="size-10 p-2" />
-          <span className="sr-only">{t('create-service')}</span>
-        </Link>
-      </Tooltip>
-
-      <Tooltip content={t('notifications')}>
-        <Link
-          to="/notifications"
-          className="rounded-full hover:bg-neutral-400/20"
-        >
-          <BellIcon className="size-10 p-2" />
-          <span className="sr-only">{t('notifications')}</span>
-        </Link>
-      </Tooltip>
-
-      <Tooltip content={t('messages')}>
-        <Link
-          to="/messages"
-          className="rounded-full hover:bg-neutral-400/20"
-        >
-          <EnvelopeIcon className="size-10 p-2" />
-          <span className="sr-only">{t('messages')}</span>
-        </Link>
-      </Tooltip>
+          <Link
+            to={action.to}
+            className="rounded-full hover:bg-neutral-400/20"
+          >
+            <action.icon className="size-10 p-2" />
+            <span className="sr-only">{action.label}</span>
+          </Link>
+        </Tooltip>
+      ))}
     </>
   );
 }
