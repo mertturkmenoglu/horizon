835703c6cf421fab3a65eb24ad7dbf2060cb70a4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-10 22:34:32 +0300

    add environment variable names as constants

---
+++ apps/api/config/vars.go
@@ -0,0 +1,13 @@
+package config
+
+const (
+	MINIO_ENDPOINT           = "minio.endpoint"
+	MINIO_USER               = "minio.user"
+	MINIO_PASSWORD           = "minio.password"
+	MINIO_BUCKETS            = "minio.buckets"
+	MINIO_LOCATION           = "minio.location"
+	MINIO_AUTOCREATE_BUCKETS = "minio.autocreate-buckets"
+	API_LOGGER_TYPE          = "api.logger.type"
+	PORT                     = "port"
+	ENV                      = "env"
+)
