7d37f0597dc8bdf2ac9b6350c563e168186ed7c7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-10 22:35:01 +0300

    add new app struct

---
+++ apps/api/internal/api/api.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"fmt"
+	"horizon/config"
+	"horizon/internal/logs"
+	"horizon/internal/upload"
+
+	"github.com/sony/sonyflake"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+type App struct {
+	Port       int
+	PortString string
+	Upload     *upload.Upload
+	Logger     *zap.Logger
+	Flake      *sonyflake.Sonyflake
+}
+
+func New() *App {
+	apiObj := &App{
+		Upload:     upload.New(),
+		Flake:      nil,
+		Logger:     logs.New(),
+		Port:       viper.GetInt(config.PORT),
+		PortString: fmt.Sprintf(":%d", viper.GetInt(config.PORT)),
+	}
+
+	flake, err := sonyflake.New(sonyflake.Settings{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	apiObj.Flake = flake
+
+	return apiObj
+}
