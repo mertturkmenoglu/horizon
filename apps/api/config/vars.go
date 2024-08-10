d87344ba8a37fd321e83eaf3f82c9051012d6553
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-11 00:02:48 +0300

    update config vars

--- apps/api/config/vars.go
+++ apps/api/config/vars.go
@@ -10,4 +10,10 @@ const (
 	API_LOGGER_TYPE          = "api.logger.type"
 	PORT                     = "port"
 	ENV                      = "env"
+	DB_HOST                  = "db.host"
+	DB_NAME                  = "db.name"
+	DB_USER                  = "db.user"
+	DB_PASSWORD              = "db.password"
+	DB_PORT                  = "db.port"
+	DB_TIMEZONE              = "db.timezone"
 )
