48f7090425c4c557c1c2a22359e5e48cc0a7bbdb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-10 22:35:31 +0300

    delete some of the old files

--- old/api/internal/esearch/indices.go
+++
@@ -1,9 +0,0 @@
-package esearch
-
-const (
-	IndexService string = "service"
-)
-
-func (s *Search) CreateIndices() {
-	s.Client.Indices.Create(IndexService).Do(s.Context)
-}
--- old/api/internal/esearch/search.go
+++
@@ -1,7 +0,0 @@
-package esearch
-
-import "github.com/elastic/go-elasticsearch/v8/typedapi/core/get"
-
-func (s *Search) GetServiceById(id string) (*get.Response, error) {
-	return s.Client.Get(IndexService, id).Do(s.Context)
-}
