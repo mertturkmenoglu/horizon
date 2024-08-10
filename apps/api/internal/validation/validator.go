8cdddab216001d093144575dfc3d0c7e5657bf9d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-10 22:35:41 +0300

    add custom validator

---
+++ apps/api/internal/validation/validator.go
@@ -0,0 +1,18 @@
+package validation
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
+
+type CustomValidator struct {
+	Validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.Validator.Struct(i); err != nil {
+		return echo.ErrBadRequest
+	}
+
+	return nil
+}
