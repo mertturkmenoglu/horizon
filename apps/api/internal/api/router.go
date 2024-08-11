92aa0a1b2c3a1aaebd5f039f57b5cfc4e4fa0999
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-11 04:03:10 +0300

    add dummy routes

--- apps/api/internal/api/router.go
+++ apps/api/internal/api/router.go
@@ -1,9 +1,45 @@
 package api
 
-import "github.com/labstack/echo/v4"
+import (
+	"context"
+	"fmt"
+	"horizon/internal/db"
+	"math/rand/v2"
+	"net/http"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/labstack/echo/v4"
+)
 
 func (app *App) RegisterRoutes() *echo.Echo {
 	e := echo.New()
+
+	e.POST("/authors", func(c echo.Context) error {
+		ctx := context.Background()
+
+		inserted, err := app.Db.Queries.CreateAuthor(ctx, db.CreateAuthorParams{
+			Name: fmt.Sprintf("Name %d", rand.IntN(100)),
+			Bio:  pgtype.Text{String: fmt.Sprintf("Bio %d", rand.IntN(100)), Valid: true},
+		})
+
+		if err != nil {
+			return echo.ErrInternalServerError.WithInternal(err)
+		}
+
+		return c.JSON(http.StatusCreated, inserted)
+	})
+
+	e.GET("/authors", func(c echo.Context) error {
+		ctx := context.Background()
+
+		authors, err := app.Db.Queries.ListAuthors(ctx)
+
+		if err != nil {
+			return echo.ErrInternalServerError.WithInternal(err)
+		}
+
+		return c.JSON(http.StatusOK, authors)
+	})
 
 	return e
 }
