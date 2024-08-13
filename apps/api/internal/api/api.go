e57fc8062d918f46ef2967bc7b003b7af0485289
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-13 15:21:08 +0300

    add setup middlewares function

--- apps/api/internal/api/api.go
+++ apps/api/internal/api/api.go
@@ -5,8 +5,14 @@ import (
 	"horizon/config"
 	"horizon/internal/db"
 	"horizon/internal/logs"
+	"horizon/internal/middlewares"
 	"horizon/internal/upload"
+	"horizon/internal/validation"
+	"time"
 
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/sony/sonyflake"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
@@ -40,4 +46,29 @@ func New() *App {
 	apiObj.Flake = flake
 
 	return apiObj
+}
+
+func SetupMiddlewares(e *echo.Echo) {
+	e.Validator = &validation.CustomValidator{
+		Validator: validator.New(),
+	}
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.RateLimiterWithConfig(middlewares.GetRateLimiterConfig()))
+
+	if viper.GetString(config.ENV) == "dev" {
+		e.IPExtractor = echo.ExtractIPDirect()
+		e.Use(middleware.RequestID())
+		e.Use(middlewares.Cors())
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: "=> ${time_rfc3339} [${method}] ${uri} (${status}) ${latency_human}\n",
+		}))
+	}
+
+	e.Use(middlewares.ZapLogger())
+	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Timeout: 10 * time.Second,
+	}))
+	e.Use(middleware.Secure())
+
 }
