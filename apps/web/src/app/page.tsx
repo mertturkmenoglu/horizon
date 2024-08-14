2517c24143f7149ee3f4a1527bd539ece4f4b3c1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-14 21:09:34 +0300

    update home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Page() {
+import FeaturesCarousel from '@/components/blocks/features-carousel';
+import QuickActions from '@/components/blocks/quick-actions';
+import ReferToFriend from '@/components/blocks/refer-to-friend';
+import { getAuth } from '@/lib/auth';
+
+export default async function Page() {
+  const auth = await getAuth();
+  const isSignedIn = auth !== null;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello World</div>
+    <main className="container">
+      {isSignedIn && (
+        <>
+          <ReferToFriend className="mt-0" />
+          <h2 className="mt-8 text-4xl font-bold">Hey {auth.data.fullName}</h2>
+          <div className="mt-2 text-lg font-semibold">How can we help you?</div>
+
+          <QuickActions className="mt-8" />
+        </>
+      )}
+
+      {/* BrowseCategoriesGrid */}
+      {/* Recommendation Grid - Popular */}
+      {/* Recommendation Grid - New */}
+
+      <FeaturesCarousel className="mx-auto mt-16 max-w-[80%] md:max-w-[70%]" />
     </main>
   );
 }
