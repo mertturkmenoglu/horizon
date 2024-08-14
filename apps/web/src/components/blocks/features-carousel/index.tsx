20e082ed3b9ee6310e227b71eaef4e4d4aa3ab48
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-14 21:07:26 +0300

    add features carousel

---
+++ apps/web/src/components/blocks/features-carousel/index.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable jsx-a11y/alt-text */
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { cn } from '@/lib/utils';
+import { items } from './items';
+
+const slogan = 'We got you covered.';
+
+type Props = {
+  className?: string;
+};
+
+export default function FeaturesCarousel({ className }: Props) {
+  return (
+    <Carousel className={cn('w-full rounded-md', className)}>
+      <CarouselContent>
+        {items.map((item) => (
+          <CarouselItem key={item.image}>
+            <div className="rounded-md p-1">
+              <div>
+                <div className="relative flex items-center justify-center rounded-md bg-black/90">
+                  <img
+                    src={item.image}
+                    className={cn(
+                      'aspect-[3] h-full rounded-md object-cover opacity-70'
+                    )}
+                    role="presentation"
+                  />
+                  <div className="absolute bottom-2 left-2 rounded-md px-8 py-4 md:bottom-8 md:left-16">
+                    <div className="text-sm font-bold text-white md:text-base">
+                      {item.text}
+                    </div>
+                    <div className="text-xl font-bold text-white md:text-3xl">
+                      {slogan}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
---
+++ apps/web/src/components/blocks/features-carousel/items.tsx
@@ -0,0 +1,22 @@
+export const items = [
+  {
+    text: 'Looking for a yoga instructor?',
+    image:
+      'https://images.unsplash.com/photo-1683130461764-af2bea342b45?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    text: 'Need help planning your next trip?',
+    image:
+      'https://images.unsplash.com/photo-1529180979161-06b8b6d6f2be?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    text: 'Cannot figure out how to publish your website?',
+    image:
+      'https://images.unsplash.com/photo-1577909659949-2302826adf26?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    text: 'Need help with accounting?',
+    image:
+      'https://images.unsplash.com/photo-1663524789638-a576264a7f53?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+];
