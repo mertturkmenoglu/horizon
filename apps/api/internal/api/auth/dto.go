7b890250e2ecff26f5808b354ca0f9310925b772
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-14 15:23:13 +0300

    add get me response dto

--- apps/api/internal/api/auth/dto.go
+++ apps/api/internal/api/auth/dto.go
@@ -1,5 +1,7 @@
 package auth
 
+import "time"
+
 type LoginRequestDto struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
@@ -10,4 +12,20 @@ type RegisterRequestDto struct {
 	Email    string `json:"email" validate:"required,email"`
 	Username string `json:"username" validate:"required"`
 	Password string `json:"password" validate:"required"`
+}
+
+type GetMeResponseDto struct {
+	ID              string    `json:"id"`
+	Email           string    `json:"email"`
+	Username        string    `json:"username"`
+	FullName        string    `json:"fullName"`
+	GoogleID        *string   `json:"googleId"`
+	IsEmailVerified bool      `json:"isEmailVerified"`
+	IsActive        bool      `json:"isActive"`
+	Role            string    `json:"role"`
+	Gender          *string   `json:"gender"`
+	ProfileImage    *string   `json:"profileImage"`
+	LastLogin       time.Time `json:"lastLogin"`
+	CreatedAt       time.Time `json:"createdAt"`
+	UpdatedAt       time.Time `json:"updatedAt"`
 }
