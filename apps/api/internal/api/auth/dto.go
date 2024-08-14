0624fbd249414cf887c736a897dbc0daebe32ddc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-14 08:06:34 +0300

    add register dto

--- apps/api/internal/api/auth/dto.go
+++ apps/api/internal/api/auth/dto.go
@@ -4,3 +4,10 @@ type LoginRequestDto struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
+
+type RegisterRequestDto struct {
+	FullName string `json:"fullName" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"password"`
+}
