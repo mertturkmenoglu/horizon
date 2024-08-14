1d0edc74816060b5edee75eb26f8d3fa43d57aa1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-14 07:10:59 +0300

    add is auth middleware

---
+++ apps/api/internal/middlewares/auth.go
@@ -0,0 +1,30 @@
+package middlewares
+
+import (
+	"horizon/internal/api/auth"
+	"horizon/internal/h"
+	"net/http"
+
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+)
+
+func IsAuth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, err := session.Get(auth.SESSION_NAME, c)
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		userId, ok := sess.Values["user_id"].(string)
+		if !ok || userId == "" {
+			return c.JSON(http.StatusUnauthorized, h.ErrResponse{
+				Message: "Invalid session state",
+			})
+		}
+
+		c.Set("auth_id", userId)
+		return next(c)
+	}
+}
