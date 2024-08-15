fdeedd6d10fd82d9c886dd7d6cf6f0e6510b619c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-15 16:30:33 +0300

    update sign in button

--- apps/web/src/components/blocks/header/sign-in-button.tsx
+++ apps/web/src/components/blocks/header/sign-in-button.tsx
@@ -1,12 +1,20 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 
 export default function SignInButton() {
   return (
-    <Button variant="default" className="rounded-full" asChild>
-      <Link href="/sign-in">Sign in</Link>
+    <Button
+      variant="default"
+      className=""
+      asChild
+    >
+      <div className="flex items-center gap-2">
+        <Link href="/sign-in">Sign in</Link>
+        <ArrowRight className="size-4" />
+      </div>
     </Button>
   );
 }
