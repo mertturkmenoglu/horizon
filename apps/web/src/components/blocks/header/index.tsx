ec0d17a3c1d95d294ea40bd0746bb9d242182b33
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-15 10:41:47 +0300

    reformat file

--- apps/web/src/components/blocks/header/index.tsx
+++ apps/web/src/components/blocks/header/index.tsx
@@ -1,10 +1,10 @@
-import Logo from "@/app/logo.png";
-import { getAuth } from "@/lib/auth";
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
-import Menu from "./menu";
-import SignInButton from "./sign-in-button";
+import Logo from '@/app/icon.png';
+import { getAuth } from '@/lib/auth';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import Menu from './menu';
+import SignInButton from './sign-in-button';
 
 type Props = React.HTMLAttributes<HTMLElement>;
 
@@ -15,13 +15,20 @@ export default async function Header({ className, ...props }: Props) {
   return (
     <header
       className={cn(
-        "container flex items-center justify-between mt-8",
+        'container mt-8 flex items-center justify-between',
         className
       )}
       {...props}
     >
-      <Link href="/" className="flex items-center gap-4">
-        <Image src={Logo} alt="Horizon" className="size-12 min-h-12 min-w-12" />
+      <Link
+        href="/"
+        className="flex items-center gap-4"
+      >
+        <Image
+          src={Logo}
+          alt="Horizon"
+          className="size-12 min-h-12 min-w-12"
+        />
 
         <div className="text-xl">Horizon</div>
       </Link>
