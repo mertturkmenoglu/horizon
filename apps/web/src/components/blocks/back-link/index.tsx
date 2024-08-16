db27c18e54964f10a408efce3b32c9de98429d30
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-16 09:00:08 +0300

    add back link component

---
+++ apps/web/src/components/blocks/back-link/index.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
+
+type Props = {
+  className?: string;
+  href: string;
+  text?: string;
+};
+
+export default function BackLink({ href, className, text = 'Go back' }: Props) {
+  return (
+    <Link
+      href={href}
+      className={cn(className)}
+    >
+      <Button
+        variant="link"
+        className="px-0"
+      >
+        <div className="flex items-center gap-2">
+          <ArrowLeft className="size-4" />
+          <div>{text}</div>
+        </div>
+      </Button>
+    </Link>
+  );
+}
