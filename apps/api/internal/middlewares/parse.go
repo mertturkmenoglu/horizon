f068314f988d4a8774daca3d8ae39700b892128c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-16 14:41:28 +0300

    update body parse middleware

--- apps/api/internal/middlewares/parse.go
+++ apps/api/internal/middlewares/parse.go
@@ -1,6 +1,9 @@
 package middlewares
 
 import (
+	"horizon/internal/h"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -13,7 +16,9 @@ func ParseBody[T any](next echo.HandlerFunc) echo.HandlerFunc {
 		}
 
 		if err := c.Validate(&body); err != nil {
-			return echo.ErrBadRequest
+			return c.JSON(http.StatusBadRequest, h.ErrResponse{
+				Message: err.Error(),
+			})
 		}
 
 		c.Set("body", body)
