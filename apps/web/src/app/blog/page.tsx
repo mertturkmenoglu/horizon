94fba2681f2fa5574e743c976a7ad5b27cda6e23
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-16 09:00:31 +0300

    update blog page

--- apps/web/src/app/blog/page.tsx
+++ apps/web/src/app/blog/page.tsx
@@ -1,13 +1,15 @@
-import AppMessage from '@/components/blocks/app-message';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default async function Page() {
   return (
-    <div>
-      <AppMessage
-        className="my-16"
-        emptyMessage="Blog Page"
-        showBackButton={false}
-      />
+    <div className="container my-32">
+      <Button
+        variant="link"
+        asChild
+      >
+        <Link href="/blog/changelog">See the changelog</Link>
+      </Button>
     </div>
   );
 }
