d1fbf07bfcbebdf0241bc92bdeca60b630ad1024
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-18 00:56:39 +0300

    add data fetching and header to the user page

--- apps/web/src/app/u/[username]/page.tsx
+++ apps/web/src/app/u/[username]/page.tsx
@@ -1,12 +1,40 @@
 import AppMessage from '@/components/blocks/app-message';
+import { Separator } from '@/components/ui/separator';
+import api from '@/lib/api';
+import { getAuthCookie } from '@/lib/auth';
+import { GetUserProfileByUsernameResponseDto } from '@/lib/dto';
+import Header from './_components/header';
 
-export default async function Page() {
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+type Props = {
+  params: {
+    username: string;
+  };
+};
+
+async function getUserProfileByUsername(username: string) {
+  const res = await api
+    .get(`users/${username}`, {
+      headers: {
+        ...getAuthCookie(),
+      },
+    })
+    .json<{ data: GetUserProfileByUsernameResponseDto }>();
+  return res.data;
+}
+
+export default async function Page({ params: { username } }: Props) {
+  const user = await getUserProfileByUsername(username);
+
   return (
-    <div>
+    <div className="container my-16">
+      <Header user={user} />
+      <Separator className="my-4" />
       <AppMessage
-        className="mt-16"
-        emptyMessage="No content"
-        showBackButton={false}
+        emptyMessage="Nothing here"
+        className="my-16"
       />
     </div>
   );
