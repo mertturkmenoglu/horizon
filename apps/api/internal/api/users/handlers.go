279cd2ce94cb83035aa61407511c1ce6e0712892
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-17 21:44:26 +0300

    implement get user profile by username handler

--- apps/api/internal/api/users/handlers.go
+++ apps/api/internal/api/users/handlers.go
@@ -1,9 +1,39 @@
 package users
 
 import (
+	"context"
+	"errors"
+	"horizon/internal/h"
+	"net/http"
+
+	"github.com/jackc/pgx/v5"
 	"github.com/labstack/echo/v4"
 )
 
-func (s *UsersService) HandlerGetUserByUsername(c echo.Context) error {
-	return nil
+func (s *UsersService) HandlerGetUserProfileByUsername(c echo.Context) error {
+	username := c.Param("username")
+
+	if username == "" {
+		return c.JSON(http.StatusBadRequest, h.ErrResponse{
+			Message: "username is required",
+		})
+	}
+
+	dbResult, err := s.Db.Queries.GetUserProfileByUsername(context.Background(), username)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return c.JSON(http.StatusNotFound, h.ErrResponse{
+				Message: "not found",
+			})
+		} else {
+			return echo.ErrInternalServerError
+		}
+	}
+
+	res := mapGetUserProfileByUsernameRowToDto(dbResult)
+
+	return c.JSON(http.StatusOK, h.Response[GetUserProfileByUsernameResponseDto]{
+		"data": res,
+	})
 }
