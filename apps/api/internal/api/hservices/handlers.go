775f706927daf63230916e7f404d573f1ef73109
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-17 10:54:01 +0300

    add logger to hservices module

--- apps/api/internal/api/hservices/handlers.go
+++ apps/api/internal/api/hservices/handlers.go
@@ -10,18 +10,21 @@ import (
 	"github.com/gosimple/slug"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/labstack/echo/v4"
+	"github.com/pterm/pterm"
 	"github.com/sony/sonyflake"
 )
 
 type HServicesService struct {
-	Db    *db.Db
-	Flake *sonyflake.Sonyflake
+	Db     *db.Db
+	Flake  *sonyflake.Sonyflake
+	Logger *pterm.Logger
 }
 
-func NewHServicesService(database *db.Db, flake *sonyflake.Sonyflake) *HServicesService {
+func NewHServicesService(database *db.Db, flake *sonyflake.Sonyflake, logger *pterm.Logger) *HServicesService {
 	return &HServicesService{
-		Db:    database,
-		Flake: flake,
+		Db:     database,
+		Flake:  flake,
+		Logger: logger,
 	}
 }
 
