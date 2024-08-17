44b6970686d93de16e196685eebba03779828dc7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-17 11:39:24 +0300

    add hservice response dto

--- apps/api/internal/api/hservices/dto.go
+++ apps/api/internal/api/hservices/dto.go
@@ -1,5 +1,7 @@
 package hservices
 
+import "github.com/jackc/pgx/v5/pgtype"
+
 type CreateHServiceRequestDto struct {
 	Title            string  `json:"title" validate:"required,min=5,max=64"`
 	Description      string  `json:"description" validate:"required,min=5,max=4096"`
@@ -13,4 +15,26 @@ type CreateHServiceRequestDto struct {
 	DeliveryTime     int     `json:"deliveryTime" validate:"required,min=1,max=32"`
 	DeliveryTimespan string  `json:"deliveryTimespan" validate:"required"`
 	Media            string  `json:"media"`
+}
+
+type GetMyHServicesResponseDto struct {
+	ID               string             `json:"id"`
+	UserID           string             `json:"userId"`
+	Title            string             `json:"title"`
+	Slug             string             `json:"slug"`
+	Description      string             `json:"description"`
+	Category         int32              `json:"category"`
+	Price            float64            `json:"price"`
+	PriceUnit        string             `json:"priceUnit"`
+	PriceTimespan    string             `json:"priceTimespan"`
+	IsOnline         bool               `json:"isOnline"`
+	Url              pgtype.Text        `json:"url"`
+	Location         string             `json:"location"`
+	DeliveryTime     int32              `json:"deliveryTime"`
+	DeliveryTimespan string             `json:"deliveryTimespan"`
+	TotalPoints      int64              `json:"totalPoints"`
+	TotalVotes       int32              `json:"totalVotes"`
+	Media            map[string]any     `json:"media"`
+	CreatedAt        pgtype.Timestamptz `json:"createdAt"`
+	UpdatedAt        pgtype.Timestamptz `json:"updatedAt"`
 }
