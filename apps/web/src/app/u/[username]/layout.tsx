37cace543e49ed2c4a8bd756834af6a7e7b231df
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-18 00:37:02 +0300

    update layout file

--- apps/web/src/app/u/[username]/layout.tsx
+++ apps/web/src/app/u/[username]/layout.tsx
@@ -5,21 +5,6 @@ type Props = {
   };
 };
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
-function Layout({ children, params: { username } }: Props): React.ReactElement {
-  return (
-    <div className="container">
-      <div>Bio: {username}</div>
-
-      <hr className="mx-auto my-4 hidden max-w-4xl md:block" />
-
-      <div>tabs navigation</div>
-
-      <div>{children}</div>
-    </div>
-  );
+export default function Layout({ children }: Props): React.ReactElement {
+  return <div>{children}</div>;
 }
-
-export default Layout;
