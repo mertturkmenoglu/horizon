23d3dc28c294987645485ca86fb6777f21e1db8a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-18 00:37:10 +0300

    update loading file

--- apps/web/src/app/u/[username]/loading.tsx
+++ apps/web/src/app/u/[username]/loading.tsx
@@ -2,13 +2,18 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Loading() {
   return (
-    <div className="mx-auto flex max-w-4xl flex-col space-y-3">
-      <Skeleton className="h-[125px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-8 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
-        <Skeleton className="h-4 w-[500px]" />
-        <Skeleton className="h-12 w-[200px]" />
+    <div className="container my-16">
+      <Skeleton className="size-24 rounded-xl" />
+      <div className="grid grid-cols-1 items-center gap-4 md:grid-cols-2">
+        <div>
+          <Skeleton className="mt-4 h-12 rounded-xl" />
+          <Skeleton className="mt-2 h-8 rounded-xl" />
+          <Skeleton className="mt-2 h-4 rounded-xl" />
+        </div>
+        <div className="flex h-32 flex-col-reverse items-stretch gap-4 md:flex-row md:justify-end">
+          <Skeleton className="h-10 w-32 rounded-xl" />
+          <Skeleton className="h-10 w-32 rounded-xl" />
+        </div>
       </div>
     </div>
   );
