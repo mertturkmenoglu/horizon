ccf4dd19feba96e0c5abe1a2b0aacc4cf95cfb72
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-18 16:54:36 +0300

    add favorite button component

---
+++ apps/web/src/app/services/[id]/_components/favorite-button.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { cn } from '@/lib/utils';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Heart } from 'lucide-react';
+import { useState } from 'react';
+import { toast } from 'sonner';
+
+type Props = {
+  isFavorite: boolean;
+  hserviceId: string;
+  isSignedIn: boolean;
+};
+
+export default function FavoriteButton({
+  isFavorite,
+  hserviceId,
+  isSignedIn,
+}: Props) {
+  const [fav, setFav] = useState(isFavorite);
+  const qc = useQueryClient();
+
+  const mutation = useMutation({
+    mutationKey: ['favorites', hserviceId],
+    mutationFn: async () => {
+      // TODO: Implement later
+    },
+    onSuccess: async () => {
+      const prev = fav;
+      setFav((prev) => !prev);
+      await qc.invalidateQueries({ queryKey: ['favorites'] });
+      toast.success(prev ? 'Removed from favorites' : 'Added to favorites');
+    },
+    onError: () => {
+      toast.error('Something went wrong');
+    },
+  });
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            onClick={() => {
+              if (!isSignedIn) {
+                toast.warning('You need to be signed in.');
+                return;
+              }
+
+              mutation.mutate();
+            }}
+          >
+            <Heart
+              className={cn('size-6 text-primary', {
+                'fill-primary': fav,
+              })}
+            />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side="bottom">
+          <p>{fav ? 'Remove favorite' : 'Add to favorites'}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
