0b2808cb051d3ab63346341f3b7068451a2e3842
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-18 11:57:04 +0300

    update hservices search schema

--- apps/api/internal/search/schemas.go
+++ apps/api/internal/search/schemas.go
@@ -7,16 +7,69 @@ import (
 
 var schemas = []*tsapi.CollectionSchema{
 	{
-		Name: "Service",
+		Name: "HService",
 		Fields: []tsapi.Field{
 			{
 				Name: "id",
 				Type: "string",
 			},
 			{
+				Name: "userId",
+				Type: "string",
+			},
+			{
 				Name: "title",
 				Type: "string",
 				Sort: pointer.True(),
+			},
+			{
+				Name: "slug",
+				Type: "string",
+			},
+			{
+				Name: "description",
+				Type: "string",
+			},
+			{
+				Name: "category",
+				Type: "int32",
+			},
+			{
+				Name: "price",
+				Type: "float",
+			},
+			{
+				Name: "priceUnit",
+				Type: "string",
+			},
+			{
+				Name: "priceTimespan",
+				Type: "string",
+			},
+			{
+				Name: "isOnline",
+				Type: "bool",
+			},
+			{
+				Name:     "url",
+				Type:     "string",
+				Optional: pointer.True(),
+			},
+			{
+				Name: "location",
+				Type: "string",
+			},
+			{
+				Name: "deliveryTime",
+				Type: "int32",
+			},
+			{
+				Name: "deliveryTimespan",
+				Type: "string",
+			},
+			{
+				Name: "media",
+				Type: "auto",
 			},
 		},
 		DefaultSortingField: pointer.String("title"),
