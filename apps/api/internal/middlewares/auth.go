c2d687c28c007d8723216475bed46d978d4f1717
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-19 14:21:15 +0300

    update auth middlewares

--- apps/api/internal/middlewares/auth.go
+++ apps/api/internal/middlewares/auth.go
@@ -9,22 +9,51 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// IsAuth checks if the user is authenticated or not.
+// If the user is authenticated, it sets the user_id in the context.
+// If the user is not authenticated, it throws an HTTP 401 StatusUnauthorized error.
 func IsAuth(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		sess, err := session.Get(auth.SESSION_NAME, c)
 
 		if err != nil {
-			return echo.ErrInternalServerError
-		}
-
-		userId, ok := sess.Values["user_id"].(string)
-		if !ok || userId == "" {
 			return c.JSON(http.StatusUnauthorized, h.ErrResponse{
 				Message: "Invalid session state",
 			})
 		}
 
+		userId, ok := sess.Values["user_id"].(string)
+		if !ok || userId == "" {
+			return c.JSON(http.StatusUnauthorized, h.ErrResponse{
+				Message: "Invalid user_id",
+			})
+		}
+
 		c.Set("user_id", userId)
+		return next(c)
+	}
+}
+
+// WithAuth checks if the user is authenticated or not.
+// If the user is authenticated, it sets the user_id in the context.
+// If the user is not authenticated, it sets the user_id to an empty string.
+func WithAuth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, err := session.Get(auth.SESSION_NAME, c)
+
+		if err != nil {
+			c.Set("user_id", "")
+			return next(c)
+		}
+
+		userId, ok := sess.Values["user_id"].(string)
+
+		if !ok || userId == "" {
+			c.Set("user_id", "")
+		} else {
+			c.Set("user_id", userId)
+		}
+
 		return next(c)
 	}
 }
