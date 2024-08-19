d2dab600a07c118898f61430af6485e7be2945ef
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-19 14:46:40 +0300

    update custom search box styles

--- apps/web/src/components/blocks/custom-search-box/index.tsx
+++ apps/web/src/components/blocks/custom-search-box/index.tsx
@@ -30,7 +30,7 @@ export default function CustomSearchBox({
       <form
         action=""
         role="search"
-        className="flex gap-2 sm:gap-4 lg:gap-8"
+        className="relative flex gap-2 sm:gap-4 lg:gap-8"
         noValidate
         onSubmit={(e) => {
           e.preventDefault();
@@ -72,10 +72,14 @@ export default function CustomSearchBox({
           onChange={(e) => {
             setQuery(e.currentTarget.value);
           }}
-          autoFocus
+          className="h-10 rounded-full"
         />
 
-        <Button type="submit">
+        <Button
+          type="submit"
+          size="lg"
+          className="absolute right-0 rounded-l-none rounded-r-full px-4"
+        >
           <span className="hidden sm:block">Search</span>
           <SearchIcon className="ml-0 size-4 sm:ml-2" />
         </Button>
