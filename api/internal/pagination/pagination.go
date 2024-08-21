172f08d9a2e25fd2c4527ad07b62ccbe73a9d334
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-21 08:16:14 +0300

    update pagination logic

--- api/internal/pagination/pagination.go
+++ api/internal/pagination/pagination.go
@@ -50,13 +50,13 @@ func GetPagination(params Params, totalRecords int64) Pagination {
 	modulo := totalRecords % int64(params.PageSize)
 	var carry int64 = 0
 
-	if modulo > 0 {
+	if modulo > 0 && totalRecords > 0 {
 		carry = 1
 	}
 
 	totalPages := totalRecords/int64(params.PageSize) + carry
-	hasPrevious := params.Page > 1
-	hasNext := int64(params.Page) < totalPages
+	hasPrevious := params.Page > 1 && totalRecords > 0
+	hasNext := int64(params.Page) < totalPages && totalRecords > 0
 
 	return Pagination{
 		Page:         params.Page,
