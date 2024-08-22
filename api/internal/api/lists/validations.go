fbd2989c86e05ecc9f2b976cc2d424faee0e912a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-22 17:16:53 +0300

    extract validation

--- api/internal/api/lists/validations.go
+++ api/internal/api/lists/validations.go
@@ -1,1 +1,9 @@
 package lists
+
+func isUserAllowedToCreateList(listCount int) bool {
+	return listCount < MaxListPerUser
+}
+
+func isUserAllowedToCreateListItem(itemCount int) bool {
+	return itemCount < MaxListItemCount
+}
