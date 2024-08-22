7f35a12bd2a0d3b54f7f8c34b60241036ca28bd3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-22 09:22:14 +0300

    update back link component

--- web/src/components/blocks/back-link/index.tsx
+++ web/src/components/blocks/back-link/index.tsx
@@ -18,6 +18,7 @@ export default function BackLink({ href, className, text = 'Go back' }: Props) {
       <Button
         variant="link"
         className="px-0"
+        asChild
       >
         <div className="flex items-center gap-2">
           <ArrowLeft className="size-4" />
