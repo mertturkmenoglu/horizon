00514acb6de59b3ab1f46a9c18ef21061df2d74c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-23 09:10:08 +0300

    add move list item after handler

--- api/internal/api/lists/handlers.go
+++ api/internal/api/lists/handlers.go
@@ -241,3 +241,122 @@ func (s *Module) HandlerDeleteListItem(c echo.Context) error {
 
 	return c.NoContent(http.StatusNoContent)
 }
+
+func (s *Module) HandlerMoveListItemAfter(c echo.Context) error {
+	userId := c.Get("user_id").(string)
+	listId := c.Param("list_id")
+	itemId := c.Param("item_id")
+	afterItemId := c.Param("after_item_id")
+
+	if listId == "" || itemId == "" || afterItemId == "" {
+		return c.JSON(http.StatusBadRequest, h.ErrResponse{
+			Message: "list_id, item_id, and after_item_id are required",
+		})
+	}
+
+	list, err := s.Db.Queries.GetListById(context.Background(), listId)
+
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+
+	if list.List.UserID != userId {
+		return c.JSON(http.StatusForbidden, h.ErrResponse{
+			Message: "you are not authorized to move this list item",
+		})
+	}
+
+	listItem, err := s.Db.Queries.GetListItemById(context.Background(), db.GetListItemByIdParams{
+		ID:     itemId,
+		ListID: listId,
+	})
+
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+
+	afterItem, err := s.Db.Queries.GetListItemById(context.Background(), db.GetListItemByIdParams{
+		ID:     afterItemId,
+		ListID: listId,
+	})
+
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+
+	if listItem.ItemOrder < afterItem.ItemOrder {
+		ctx := context.Background()
+		tx, err := s.Db.Pool.Begin(ctx)
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		defer tx.Rollback(ctx)
+
+		qtx := s.Db.Queries.WithTx(tx)
+
+		err = qtx.UpdateListItemOrderWithIndexRangeDecr(ctx, db.UpdateListItemOrderWithIndexRangeDecrParams{
+			ListID:      listId,
+			ItemOrder:   listItem.ItemOrder + 1,
+			ItemOrder_2: afterItem.ItemOrder,
+		})
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		err = qtx.UpdateListItemOrder(ctx, db.UpdateListItemOrderParams{
+			ID:        itemId,
+			ItemOrder: afterItem.ItemOrder,
+		})
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		err = tx.Commit(ctx)
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+	} else {
+		ctx := context.Background()
+		tx, err := s.Db.Pool.Begin(ctx)
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		defer tx.Rollback(ctx)
+
+		qtx := s.Db.Queries.WithTx(tx)
+
+		err = qtx.UpdateListItemOrderWithIndexRangeIncr(ctx, db.UpdateListItemOrderWithIndexRangeIncrParams{
+			ListID:      listId,
+			ItemOrder:   afterItem.ItemOrder + 1,
+			ItemOrder_2: listItem.ItemOrder - 1,
+		})
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		err = qtx.UpdateListItemOrder(ctx, db.UpdateListItemOrderParams{
+			ID:        itemId,
+			ItemOrder: afterItem.ItemOrder + 1,
+		})
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		err = tx.Commit(ctx)
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
