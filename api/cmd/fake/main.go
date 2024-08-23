ae7fb9d6ea5d72dfb50cda62ad6127ad4128d588
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-23 07:29:54 +0300

    update fake data generation

--- api/cmd/fake/main.go
+++ api/cmd/fake/main.go
@@ -1,37 +1,23 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 	"horizon/config"
-	"horizon/internal/api/hservices"
 	"horizon/internal/db"
-	"horizon/internal/hash"
 	"strconv"
-	"time"
 
 	"github.com/brianvoe/gofakeit/v7"
-	"github.com/gosimple/slug"
-	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/pterm/pterm"
 )
-
-var database *db.Db
-var logger *pterm.Logger
-
-func GetDb() *db.Db {
-	if database == nil {
-		database = db.NewDb()
-	}
-	return database
-}
 
 func main() {
 	// Load .env variables and other configurations
 	config.Bootstrap()
 
 	logger = pterm.DefaultLogger.WithLevel(pterm.LogLevelTrace)
+
+	pterm.DefaultBasicText.Println("You can use the" + pterm.LightYellow(" arrow keys ") + "to navigate between options.")
+
 	genType, _ := pterm.DefaultInteractiveSelect.WithOptions(genOptions).Show()
 	sCount, _ := pterm.DefaultInteractiveTextInput.Show("How many do you want to generate? (Between 1 and 10 000)")
 	count, err := strconv.ParseInt(sCount, 10, 32)
@@ -59,115 +45,28 @@ func generateAndInsert(genType string, count int) error {
 		return handleUser(count)
 	case "hservice":
 		return handleHService(count)
+	case "bookmarks":
+		return handleBookmarks(count)
+	case "favorites":
+		return handleFavorites(count)
+	case "lists":
+		return handleLists(count)
+	case "list-items":
+		return handleListItems(count)
 	default:
 		logger.Error("Invalid generating type.")
 		return nil
 	}
 }
 
-func handleUser(count int) error {
-	ctx := context.Background()
-	d := GetDb()
-	h, err := hash.Hash("LoremIpsum!1")
+var database *db.Db
+var logger *pterm.Logger
 
-	if err != nil {
-		return err
+func GetDb() *db.Db {
+	if database == nil {
+		database = db.NewDb()
 	}
-
-	for i := 0; i < count; i++ {
-		logger.Debug("Generating user", logger.Args("i", i))
-
-		_, err = d.Queries.CreateUser(ctx, db.CreateUserParams{
-			ID:              gofakeit.UUID(),
-			Email:           gofakeit.Email(),
-			Username:        gofakeit.Username(),
-			FullName:        gofakeit.Name(),
-			PasswordHash:    pgtype.Text{String: h, Valid: true},
-			GoogleID:        pgtype.Text{Valid: false},
-			IsEmailVerified: gofakeit.Bool(),
-			ProfileImage:    pgtype.Text{String: gofakeit.RandomString(imageUrls), Valid: true},
-		})
-
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func handleHService(count int) error {
-	ctx := context.Background()
-	d := GetDb()
-
-	userId, err := pterm.DefaultInteractiveTextInput.Show("We need a user id. Enter a valid user ID that is in your database")
-
-	if err != nil {
-		return err
-	}
-
-	for i := 0; i < count; i++ {
-		logger.Debug("Generating hservice", logger.Args("i", i))
-
-		title := gofakeit.ProductName()
-		slugVal := slug.Make(title)
-		totalVotes := gofakeit.IntRange(5, 1000)
-		totalPoints := gofakeit.IntRange(totalVotes, totalVotes*5)
-		mediaCount := gofakeit.IntRange(3, 10)
-		allMedia := make([]any, 0)
-
-		for j := 0; j < mediaCount; j++ {
-			media := map[string]any{
-				"type":      "image",
-				"url":       GetRandomImageUrl(),
-				"thumbnail": GetRandomImageUrl(),
-				"alt":       gofakeit.Sentence(3),
-				"caption":   gofakeit.Sentence(5),
-				"width":     gofakeit.IntRange(100, 1920),
-				"height":    gofakeit.IntRange(100, 1080),
-			}
-
-			allMedia = append(allMedia, media)
-		}
-
-		mediaData := map[string]any{
-			"data": allMedia,
-		}
-
-		mediaBytes, err := json.Marshal(mediaData)
-
-		if err != nil {
-			return err
-		}
-
-		_, err = d.Queries.CreateHService(ctx, db.CreateHServiceParams{
-			ID:               gofakeit.UUID(),
-			UserID:           userId,
-			Title:            title,
-			Slug:             slugVal,
-			Description:      gofakeit.ProductDescription(),
-			Category:         int32(gofakeit.IntRange(1, 32)),
-			Price:            gofakeit.Price(1, 10000),
-			PriceUnit:        db.Priceunit(gofakeit.RandomString(hservices.ValidPriceUnits)),
-			PriceTimespan:    db.Worktimespan(gofakeit.RandomString(hservices.ValidTimespans)),
-			IsOnline:         gofakeit.Bool(),
-			Url:              pgtype.Text{String: gofakeit.URL(), Valid: true},
-			Location:         gofakeit.Address().Address,
-			DeliveryTime:     int32(gofakeit.IntRange(1, 32)),
-			DeliveryTimespan: db.Worktimespan(gofakeit.RandomString(hservices.ValidTimespans)),
-			TotalVotes:       int32(totalVotes),
-			TotalPoints:      int64(totalPoints),
-			CreatedAt:        pgtype.Timestamptz{Time: time.Now(), Valid: true},
-			UpdatedAt:        pgtype.Timestamptz{Time: time.Now(), Valid: true},
-			Media:            mediaBytes,
-		})
-
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return database
 }
 
 var imageUrls = []string{
@@ -203,8 +102,8 @@ var imageUrls = []string{
 	"https://imgur.com/mWzmPRv",
 }
 
-var genOptions = []string{"user", "hservice"}
+var genOptions = []string{"user", "hservice", "bookmarks", "favorites", "lists", "list-items"}
 
 func GetRandomImageUrl() string {
-	return fmt.Sprintf("https://loremflickr.com/960/720?lock=%d", gofakeit.IntRange(1, 1000))
+	return fmt.Sprintf("https://loremflickr.com/960/720?lock=%d", gofakeit.IntRange(1, 10_000))
 }
