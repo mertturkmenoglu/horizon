e4b12d2e573b24e2f74e7b17b5988cfcc40a9c8a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-27 00:24:37 +0300

    update reviews handlers

--- api/internal/api/reviews/handlers.go
+++ api/internal/api/reviews/handlers.go
@@ -1,257 +1,143 @@
 package reviews
 
 import (
-	"context"
-	"errors"
-	"horizon/internal/db"
 	"horizon/internal/h"
 	"horizon/internal/pagination"
 	"net/http"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/labstack/echo/v4"
 )
 
-func (s *Module) HandlerGetReviewsByHServiceId(c echo.Context) error {
+func (s *handlers) getReviewsByHServiceId(c echo.Context) error {
 	id := c.Param("id")
 
 	if id == "" {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: "id is required",
-		})
+		return echo.NewHTTPError(http.StatusBadRequest, errIdRequired)
 	}
 
 	params, err := pagination.GetParamsFromContext(c)
 
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: err.Error(),
-		})
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	dbResult, err := s.Db.Queries.GetReviewsByHServiceId(context.Background(), db.GetReviewsByHServiceIdParams{
-		HserviceID: id,
-		Offset:     int32(params.Offset),
-		Limit:      int32(params.PageSize),
-	})
+	res, pagination, err := s.service.getHServiceReviews(id, params)
 
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return c.JSON(http.StatusNotFound, h.ErrResponse{
-				Message: "not found",
-			})
-		}
-
-		return echo.ErrInternalServerError
-	}
-
-	count, err := s.Db.Queries.CountReviewsByHServiceId(context.Background(), id)
-
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return c.JSON(http.StatusNotFound, h.ErrResponse{
-				Message: "not found",
-			})
-		}
-
-		return echo.ErrInternalServerError
-	}
-
-	paginationData := pagination.GetPagination(params, count)
-
-	res, err := mapGetReviewsByHServiceIdToDto(dbResult)
-
-	if err != nil {
-		return echo.ErrInternalServerError
+		return h.HandleDbErr(c, err)
 	}
 
 	return c.JSON(http.StatusOK, h.PaginatedResponse[GetReviewsByHServiceIdResponseDto]{
-		Data:       res,
-		Pagination: paginationData,
+		Data:       *res,
+		Pagination: *pagination,
 	})
 }
 
-func (s *Module) HandlerGetReviewsByUsername(c echo.Context) error {
+func (s *handlers) getReviewsByUsername(c echo.Context) error {
 	username := c.Param("username")
 
 	if username == "" {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: "username is required",
-		})
+		return echo.NewHTTPError(http.StatusBadRequest, errUsernameRequired)
 	}
 
 	params, err := pagination.GetParamsFromContext(c)
 
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: err.Error(),
-		})
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	dbResult, err := s.Db.Queries.GetReviewsByUsername(context.Background(), db.GetReviewsByUsernameParams{
-		Username: username,
-		Offset:   int32(params.Offset),
-		Limit:    int32(params.PageSize),
-	})
+	res, pagination, err := s.service.getUserReviews(username, params)
 
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return c.JSON(http.StatusNotFound, h.ErrResponse{
-				Message: "not found",
-			})
-		}
-
-		return echo.ErrInternalServerError
+		return h.HandleDbErr(c, err)
 	}
 
-	res, err := mapGetReviewsByUsernameToDto(dbResult)
-
-	if err != nil {
-		return echo.ErrInternalServerError
-	}
-
-	return c.JSON(http.StatusOK, h.Response[GetReviewsByUsernameResponseDto]{
-		Data: res,
+	return c.JSON(http.StatusOK, h.PaginatedResponse[GetReviewsByUsernameResponseDto]{
+		Data:       *res,
+		Pagination: *pagination,
 	})
 }
 
-func (s *Module) HandlerGetReviewById(c echo.Context) error {
+func (s *handlers) getReviewById(c echo.Context) error {
 	id := c.Param("id")
 
 	if id == "" {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: "id is required",
-		})
+		return echo.NewHTTPError(http.StatusBadRequest, errIdRequired)
 	}
 
-	dbResult, err := s.Db.Queries.GetReviewById(context.Background(), id)
+	res, err := s.service.getReview(id)
 
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return c.JSON(http.StatusNotFound, h.ErrResponse{
-				Message: "not found",
-			})
-		}
-
-		return echo.ErrInternalServerError
-	}
-
-	res, err := mapGetReviewByIdToDto(dbResult)
-
-	if err != nil {
-		return echo.ErrInternalServerError
+		return h.HandleDbErr(c, err)
 	}
 
 	return c.JSON(http.StatusOK, h.Response[ReviewItemDto]{
-		Data: res,
+		Data: *res,
 	})
 }
 
-func (s *Module) HandlerCreateReview(c echo.Context) error {
+func (s *handlers) createReview(c echo.Context) error {
 	userId := c.Get("user_id").(string)
 	dto := c.Get("body").(CreateReviewRequestDto)
 
-	dbResult, err := s.Db.Queries.CreateReview(context.Background(), db.CreateReviewParams{
-		ID:         h.GenerateId(s.Flake),
-		UserID:     userId,
-		HserviceID: dto.HServiceID,
-		Rating:     dto.Rating,
-		Comment:    dto.Comment,
-		Media:      []byte(dto.Media),
-	})
+	res, err := s.service.createReview(userId, dto)
 
 	if err != nil {
-		return echo.ErrInternalServerError
-	}
-
-	res, err := mapCreateReviewToDto(dbResult)
-
-	if err != nil {
-		return echo.ErrInternalServerError
+		return h.HandleDbErr(c, err)
 	}
 
 	return c.JSON(http.StatusCreated, h.Response[CreateReviewResponseDto]{
-		Data: res,
+		Data: *res,
 	})
 }
 
-func (s *Module) HandlerDeleteReview(c echo.Context) error {
+func (s *handlers) deleteReview(c echo.Context) error {
 	userId := c.Get("user_id").(string)
 	id := c.Param("id")
 
 	if id == "" {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: "id is required",
-		})
+		return echo.NewHTTPError(http.StatusBadRequest, errIdRequired)
 	}
 
-	err := s.Db.Queries.DeleteReviewById(context.Background(), db.DeleteReviewByIdParams{
-		ID:     id,
-		UserID: userId,
-	})
+	err := s.service.deleteReview(id, userId)
 
 	if err != nil {
-		return echo.ErrBadRequest
+		return h.HandleDbErr(c, err)
 	}
 
 	return c.NoContent(http.StatusNoContent)
 }
 
-func (s *Module) HandlerCreateReviewVote(c echo.Context) error {
+func (s *handlers) createReviewVote(c echo.Context) error {
 	userId := c.Get("user_id").(string)
 	id := c.Param("id")
 
 	if id == "" {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: "id is required",
-		})
+		return echo.NewHTTPError(http.StatusBadRequest, errIdRequired)
 	}
 
 	dto := c.Get("body").(CreateReviewVoteRequestDto)
-
-	var reviewVoteType db.Reviewvotetype = db.ReviewvotetypeLIKE
-
-	if dto.VoteType == "DISLIKE" {
-		reviewVoteType = db.ReviewvotetypeDISLIKE
-	} else if dto.VoteType == "LIKE" {
-		reviewVoteType = db.ReviewvotetypeLIKE
-	} else {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: "voteType is invalid",
-		})
-	}
-
-	_, err := s.Db.Queries.CreateReviewVote(context.Background(), db.CreateReviewVoteParams{
-		ID:       h.GenerateId(s.Flake),
-		UserID:   userId,
-		ReviewID: id,
-		VoteType: reviewVoteType,
-	})
+	err := s.service.createReviewVote(id, userId, dto)
 
 	if err != nil {
-		return echo.ErrBadRequest
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
 	return c.NoContent(http.StatusCreated)
 }
 
-func (s *Module) HandlerDeleteReviewVote(c echo.Context) error {
+func (s *handlers) deleteReviewVote(c echo.Context) error {
 	userId := c.Get("user_id").(string)
 	id := c.Param("id")
 
 	if id == "" {
-		return c.JSON(http.StatusBadRequest, h.ErrResponse{
-			Message: "id is required",
-		})
+		return echo.NewHTTPError(http.StatusBadRequest, errIdRequired)
 	}
 
-	err := s.Db.Queries.DeleteReviewVoteById(context.Background(), db.DeleteReviewVoteByIdParams{
-		ID:     id,
-		UserID: userId,
-	})
+	err := s.service.deleteReviewVote(id, userId)
 
 	if err != nil {
-		return echo.ErrBadRequest
+		return h.HandleDbErr(c, err)
 	}
 
 	return c.NoContent(http.StatusNoContent)
