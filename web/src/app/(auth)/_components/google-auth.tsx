d90b2b1831519ca131de03eea2fe94d8ef629591
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-28 00:00:28 +0300

    refactor: use useCallback for onClick handler in GoogleAuth component for better performance and readability

--- web/src/app/(auth)/_components/google-auth.tsx
+++ web/src/app/(auth)/_components/google-auth.tsx
@@ -1,20 +1,23 @@
 import { Button } from '@/components/ui/button';
 import GoogleIcon from '@/components/ui/google-icon';
+import { useCallback } from 'react';
 
 type Props = {
   text: string;
 };
 
-export default function GoogleAuth({ text }: Props) {
+export default function GoogleAuth({ text }: Readonly<Props>) {
   const baseUrl = process.env.NEXT_PUBLIC_API ?? '';
+
+  const onClick = useCallback(() => {
+    window.location.href = baseUrl + '/auth/google';
+  }, [baseUrl]);
 
   return (
     <Button
       variant="outline"
       className="w-full"
-      onClick={() => {
-        window.location.href = baseUrl + '/auth/google';
-      }}
+      onClick={onClick}
       type="button"
     >
       <GoogleIcon className="mr-2 size-5" />
