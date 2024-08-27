50a89a34f14b148184c1231657f1148fecbcbe84
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-28 00:02:04 +0300

    refactor middleware

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,5 +1,5 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
@@ -7,18 +7,19 @@ export function middleware(request: NextRequest) {
   // the public path should not be visible when the user has the token
   // the private path should not be visible when the user doesn't have the token
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === "/sign-in" || path === "/sign-up";
-  const token = request.cookies.get("token")?.value || ""; // check if the token exists
+  const isPublicPath = path === '/sign-in' || path === '/sign-up';
+  // check if the token exists
+  const token = request.cookies.get('token')?.value ?? '';
   if (isPublicPath && token.length > 0) {
     // redirect them to home page
-    return NextResponse.redirect(new URL("/", request.nextUrl));
+    return NextResponse.redirect(new URL('/', request.nextUrl));
   }
-  if (!isPublicPath && !(token.length > 0)) {
+  if (!isPublicPath && token.length <= 0) {
     // redirect them to the login page
-    return NextResponse.redirect(new URL("/sign-in", request.nextUrl));
+    return NextResponse.redirect(new URL('/sign-in', request.nextUrl));
   }
 }
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/profile", "/sign-in", "/sign-up"],
+  matcher: ['/profile', '/sign-in', '/sign-up'],
 };
