09efcce7f56411f1863f4d967a404b9619a31718
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-28 01:09:00 +0300

    add new random string generator helper

--- api/internal/random/random.go
+++ api/internal/random/random.go
@@ -2,6 +2,8 @@ package random
 
 import (
 	"crypto/rand"
+	"fmt"
+	"math/big"
 	randv1 "math/rand"
 )
 
@@ -26,4 +28,16 @@ func GenerateBytes(n uint32) ([]byte, error) {
 	}
 
 	return arr, nil
+}
+
+// Generate 6 digits random string code
+func GenerateSixDigitsCode() (string, error) {
+	n, err := rand.Int(rand.Reader, big.NewInt(999999))
+
+	if err != nil {
+		return "", err
+	}
+
+	s := fmt.Sprintf("%06d", n)
+	return s, nil
 }
