6274b7ecf843e6bd9f42a143b858ad879c8bcfc6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-29 14:05:31 +0300

    add swagger docs comments to health module

--- api/internal/api/health/dto.go
+++ api/internal/api/health/dto.go
@@ -1,5 +1,8 @@
 package health
 
+// GetHealthResponseDto godoc
+//
+// @Description	Generic response dto
 type GetHealthResponseDto struct {
 	Message string `json:"message"`
 }
--- api/internal/api/health/handlers.go
+++ api/internal/api/health/handlers.go
@@ -6,6 +6,15 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// GetHealth godoc
+//
+//	@Summary		Returns ok message
+//	@Description	An endpoint to be used by load balancers to check the health of the service.
+//	@Tags			health
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	GetHealthResponseDto
+//	@Router			/health/ [get]
 func (h *handlers) getHealth(c echo.Context) error {
 	return c.JSON(http.StatusOK, GetHealthResponseDto{
 		Message: "OK",
