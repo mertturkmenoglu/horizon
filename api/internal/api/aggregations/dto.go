de928e7b5024d305a39260069a57a31ab6704898
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-29 14:27:02 +0300

    add swagger comments

--- api/internal/api/aggregations/dto.go
+++ api/internal/api/aggregations/dto.go
@@ -4,38 +4,45 @@ import (
 	"time"
 )
 
+// UserResponseDto
+// @Description Basic user information
 type UserResponseDto struct {
-	ID           string    `json:"id"`
-	Username     string    `json:"username"`
-	FullName     string    `json:"fullName"`
-	Gender       *string   `json:"gender"`
-	ProfileImage *string   `json:"profileImage"`
-	CreatedAt    time.Time `json:"createdAt"`
+	ID           string    `json:"id" example:"528696135489945615"`
+	Username     string    `json:"username" example:"johndoe"`
+	FullName     string    `json:"fullName" example:"John Doe"`
+	Gender       *string   `json:"gender" example:"male"`
+	ProfileImage *string   `json:"profileImage" example:"https://example.com/image.jpg"`
+	CreatedAt    time.Time `json:"createdAt" example:"2024-08-26T10:24:13.508676+03:00"`
 }
 
+// HServiceResponseDto
+// @Description Basic service information
+// @Description with user information
 type HServiceResponseDto struct {
-	ID               string          `json:"id"`
-	UserID           string          `json:"userId"`
+	ID               string          `json:"id" example:"7234882566245847040"`
+	UserID           string          `json:"userId" example:"528696135489945615"`
 	User             UserResponseDto `json:"user"`
-	Title            string          `json:"title"`
-	Slug             string          `json:"slug"`
-	Description      string          `json:"description"`
-	Category         int32           `json:"category"`
-	Price            float64         `json:"price"`
-	PriceUnit        string          `json:"priceUnit"`
-	PriceTimespan    string          `json:"priceTimespan"`
-	IsOnline         bool            `json:"isOnline"`
-	Url              *string         `json:"url"`
-	Location         string          `json:"location"`
-	DeliveryTime     int32           `json:"deliveryTime"`
-	DeliveryTimespan string          `json:"deliveryTimespan"`
-	TotalPoints      int64           `json:"totalPoints"`
-	TotalVotes       int32           `json:"totalVotes"`
+	Title            string          `json:"title" example:"Example Service"`
+	Slug             string          `json:"slug" example:"example-service"`
+	Description      string          `json:"description" example:"Example service description"`
+	Category         int32           `json:"category" example:"1"`
+	Price            float64         `json:"price" example:"10.00"`
+	PriceUnit        string          `json:"priceUnit" example:"USD"`
+	PriceTimespan    string          `json:"priceTimespan" example:"HOURLY"`
+	IsOnline         bool            `json:"isOnline" example:"true"`
+	Url              *string         `json:"url" example:"https://example.com"`
+	Location         string          `json:"location" example:"Example Location"`
+	DeliveryTime     int32           `json:"deliveryTime" example:"1"`
+	DeliveryTimespan string          `json:"deliveryTimespan" example:"HOURLY"`
+	TotalPoints      int64           `json:"totalPoints" example:"50"`
+	TotalVotes       int32           `json:"totalVotes" example:"10"`
 	Media            map[string]any  `json:"media"`
-	CreatedAt        time.Time       `json:"createdAt"`
-	UpdatedAt        time.Time       `json:"updatedAt"`
+	CreatedAt        time.Time       `json:"createdAt" example:"2024-08-26T10:24:13.508676+03:00"`
+	UpdatedAt        time.Time       `json:"updatedAt" example:"2024-08-26T10:24:13.508676+03:00"`
 }
 
+// GetHomeAggregationsResponseDto
+// @Description Response for home aggregations
 type GetHomeAggregationsResponseDto struct {
 	New       []HServiceResponseDto `json:"new"`
 	Popular   []HServiceResponseDto `json:"popular"`
