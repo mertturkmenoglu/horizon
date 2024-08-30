4df32e92bf630efb2f60c3ceb1baaa6ec832d29e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-30 12:03:22 +0300

    add Swagger annotations for response types in response.go

--- api/internal/h/response.go
+++ api/internal/h/response.go
@@ -11,22 +11,37 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// Response
+// @Description Response
+type R struct {
+	Data any `json:"data" validate:"required"`
+} //@name HR
+
+// Paginated Response
+// @Description Paginated Response
+type PR struct {
+	Data       any                   `json:"data" validate:"required"`
+	Pagination pagination.Pagination `json:"pagination" validate:"required"`
+} //@name HPR
+
+// Metadata Response
+// @Description Metadata Response
+type MR struct {
+	Data any `json:"data" validate:"required"`
+	Meta any `json:"metadata" validate:"required"`
+} //@name HMR
+
 // Utility type to wrap arbitrary JSON responses
 type Response[T any] struct {
 	Data T `json:"data"`
-}
-
-type MetadataResponse[T any, M any] struct {
-	Data T `json:"data"`
-	Meta M `json:"metadata"`
-}
+} //@name HResponse
 
 // A PaginatedResponse is similar to [horizon/internal/h/Response] type but it is used for
 // returning paginated data.
 type PaginatedResponse[T any] struct {
 	Data       T                     `json:"data"`
 	Pagination pagination.Pagination `json:"pagination"`
-}
+} //@name HPaginatedResponse
 
 // An AnyResponse type is usually used to return an arbitrary map value and it doesn't require
 // you to specify the data type.
